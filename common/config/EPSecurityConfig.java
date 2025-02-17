787f5ce99d662b584cb36fba899a0d90a968709e
Akila Sachintha <akila.silva@rootcode.io>
2025-02-17 09:57:01 +0530

    Feat: #372 - ep added for stripe apis

--- common/config/EPSecurityConfig.java
+++ common/config/EPSecurityConfig.java
@@ -86,7 +86,7 @@ public class EPSecurityConfig {
 						"/v1/ep/organization/login-method", "/v1/ep/auth/password-reset",
 						"/v1/ep/auth/password-reset/verify-otp", "/v1/ep/auth/password-reset/send-otp",
 						"/v1/ep/auth/password-reset/resend-otp", "/v1/ep/auth/tenant/availability",
-						"/v1/google-calendar/redirect", "/v1/validate/email", "/v1/stripe/webhook")
+						"/v1/google-calendar/redirect", "/v1/validate/email", "/v1/ep/stripe/webhook")
 				.permitAll()
 				.requestMatchers("/v1/reset-database")
 				.permitAll()
