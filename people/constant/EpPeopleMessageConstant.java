d4d8dd1d07848f03ba643543d1d97747375fc2e7
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-02-20 13:14:17 +0530

    feat/472: employee bulk upload pro limit handle

---
+++ people/constant/EpPeopleMessageConstant.java
@@ -0,0 +1,15 @@
+package com.skapp.enterprise.people.constant;
+
+import com.skapp.community.common.constant.MessageConstant;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum EpPeopleMessageConstant implements MessageConstant {
+
+    EP_PEOPLE_ERROR_ALLOWED_USER_LIMIT_EXCEEDED("ep.people.error.allowed.user.limit.exceeded");
+
+    private final String messageKey;
+
+}
--- people/service/impl/EpPeopleServiceImpl.java
+++ people/service/impl/EpPeopleServiceImpl.java
@@ -1,25 +1,60 @@
 package com.skapp.enterprise.people.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skapp.community.common.payload.response.ResponseEntityDto;
+import com.skapp.community.common.repository.UserDao;
+import com.skapp.community.common.service.BulkContextService;
+import com.skapp.community.common.service.EncryptionDecryptionService;
+import com.skapp.community.common.service.UserService;
+import com.skapp.community.common.service.impl.AsyncEmailServiceImpl;
 import com.skapp.community.common.type.Role;
+import com.skapp.community.common.util.MessageUtil;
+import com.skapp.community.common.util.transformer.PageTransformer;
+import com.skapp.community.peopleplanner.mapper.PeopleMapper;
+import com.skapp.community.peopleplanner.payload.request.EmployeeBulkDto;
+import com.skapp.community.peopleplanner.payload.response.EmployeeBulkResponseDto;
 import com.skapp.community.peopleplanner.repository.EmployeeDao;
+import com.skapp.community.peopleplanner.repository.EmployeeEducationDao;
+import com.skapp.community.peopleplanner.repository.EmployeeFamilyDao;
+import com.skapp.community.peopleplanner.repository.EmployeeManagerDao;
+import com.skapp.community.peopleplanner.repository.EmployeePeriodDao;
+import com.skapp.community.peopleplanner.repository.EmployeeProgressionDao;
 import com.skapp.community.peopleplanner.repository.EmployeeRoleDao;
+import com.skapp.community.peopleplanner.repository.EmployeeTeamDao;
+import com.skapp.community.peopleplanner.repository.EmployeeTimelineDao;
+import com.skapp.community.peopleplanner.repository.EmployeeVisaDao;
+import com.skapp.community.peopleplanner.repository.JobFamilyDao;
+import com.skapp.community.peopleplanner.repository.JobTitleDao;
+import com.skapp.community.peopleplanner.repository.TeamDao;
+import com.skapp.community.peopleplanner.service.EmployeeTimelineService;
+import com.skapp.community.peopleplanner.service.PeopleEmailService;
+import com.skapp.community.peopleplanner.service.RolesService;
+import com.skapp.community.peopleplanner.service.impl.PeopleServiceImpl;
 import com.skapp.community.peopleplanner.type.AccountStatus;
+import com.skapp.enterprise.common.config.TenantContext;
 import com.skapp.enterprise.common.config.TenantValidator;
 import com.skapp.enterprise.common.constant.EpCommonConstants;
+import com.skapp.enterprise.common.masterrepository.TenantDao;
+import com.skapp.enterprise.common.model.master.Tenant;
 import com.skapp.enterprise.common.repository.EpEmployeeRoleRepository;
+import com.skapp.enterprise.common.type.Tier;
+import com.skapp.enterprise.people.constant.EpPeopleMessageConstant;
 import com.skapp.enterprise.people.payload.response.EpEmployeeRoleLimitDto;
 import com.skapp.enterprise.people.service.EpPeopleService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.List;
 import java.util.Set;
 
-@RequiredArgsConstructor
 @Slf4j
 @Service
-public class EpPeopleServiceImpl implements EpPeopleService {
+@Primary
+public class EpPeopleServiceImpl extends PeopleServiceImpl implements EpPeopleService {
 
 	private final EmployeeDao employeeDao;
 
@@ -28,6 +63,23 @@ public class EpPeopleServiceImpl implements EpPeopleService {
 	private final TenantValidator tenantValidator;
 
 	private final EpEmployeeRoleRepository epEmployeeRoleRepository;
+
+	private final TenantDao tenantDao;
+
+	private final TenantContext tenantContext;
+
+	private final MessageUtil messageUtil;
+
+	public EpPeopleServiceImpl(UserService userService, MessageUtil messageUtil, PeopleMapper peopleMapper, UserDao userDao, TeamDao teamDao, EmployeeDao employeeDao, JobFamilyDao jobFamilyDao, EmployeeProgressionDao employeeProgressionDao, JobTitleDao jobTitleDao, EmployeePeriodDao employeePeriodDao, EmployeeVisaDao employeeVisaDao, EmployeeEducationDao employeeEducationDao, EmployeeFamilyDao employeeFamilyDao, EmployeeTeamDao employeeTeamDao, EmployeeTimelineDao employeeTimelineDao, EmployeeManagerDao employeeManagerDao, EmployeeTimelineService employeeTimelineService, PasswordEncoder passwordEncoder, RolesService rolesService, PageTransformer pageTransformer, PlatformTransactionManager transactionManager, PeopleEmailService peopleEmailService, ObjectMapper mapper, EncryptionDecryptionService encryptionDecryptionService, BulkContextService bulkContextService, AsyncEmailServiceImpl asyncEmailServiceImpl, ApplicationEventPublisher applicationEventPublisher, EmployeeDao employeeDao1, EmployeeRoleDao employeeRoleDao, TenantValidator tenantValidator, EpEmployeeRoleRepository epEmployeeRoleRepository, TenantDao tenantDao, TenantContext tenantContext, MessageUtil messageUtil1) {
+		super(userService, messageUtil, peopleMapper, userDao, teamDao, employeeDao, jobFamilyDao, employeeProgressionDao, jobTitleDao, employeePeriodDao, employeeVisaDao, employeeEducationDao, employeeFamilyDao, employeeTeamDao, employeeTimelineDao, employeeManagerDao, employeeTimelineService, passwordEncoder, rolesService, pageTransformer, transactionManager, peopleEmailService, mapper, encryptionDecryptionService, bulkContextService, asyncEmailServiceImpl, applicationEventPublisher);
+        this.employeeDao = employeeDao1;
+        this.employeeRoleDao = employeeRoleDao;
+        this.tenantValidator = tenantValidator;
+        this.epEmployeeRoleRepository = epEmployeeRoleRepository;
+        this.tenantDao = tenantDao;
+        this.tenantContext = tenantContext;
+        this.messageUtil = messageUtil1;
+    }
 
 	@Override
 	public ResponseEntityDto getEmployeesLimit() {
@@ -59,6 +111,38 @@ public class EpPeopleServiceImpl implements EpPeopleService {
 	public ResponseEntityDto getEmployeeRoleLimit() {
 		EpEmployeeRoleLimitDto roleLimits = checkEmployeeRoleLimits();
 		return new ResponseEntityDto(false, roleLimits);
+	}
+
+	@Override
+	protected List<EmployeeBulkDto> getValidEmployeeBulkDtoList(List<EmployeeBulkDto> employeeBulkDtoList) {
+		String tenantId = TenantContext.getCurrentTenant();
+		tenantContext.setTenantAndSwitchSchema(EpCommonConstants.MASTER_DATABASE);
+		Tenant tenant = tenantDao.findByTenantName(tenantId);
+		tenantContext.setTenantAndSwitchSchema(tenantId);
+
+		if (tenant.getTier() == Tier.PRO) {
+			long employeeCount = countActiveAndPendingEmployees();
+			long maxAllowedCount = tenant.getSubscriptionQuantity() - employeeCount;
+
+			if (maxAllowedCount < employeeBulkDtoList.size()) {
+				return employeeBulkDtoList.subList(0, (int) Math.min(maxAllowedCount, employeeBulkDtoList.size()));
+			}
+		}
+
+		return employeeBulkDtoList;
+	}
+
+	@Override
+	protected List<EmployeeBulkResponseDto> getTotalResultList(List<EmployeeBulkResponseDto> results,
+															   List<EmployeeBulkDto> overflowedEmployeeBulkDtoList) {
+
+		for (EmployeeBulkDto overflowedEmployee : overflowedEmployeeBulkDtoList) {
+			EmployeeBulkResponseDto employeeBulkResponseDto = createErrorResponse(overflowedEmployee,
+					messageUtil.getMessage(EpPeopleMessageConstant.EP_PEOPLE_ERROR_ALLOWED_USER_LIMIT_EXCEEDED));
+			results.add(employeeBulkResponseDto);
+		}
+
+		return results;
 	}
 
 	private EpEmployeeRoleLimitDto checkEmployeeRoleLimits() {
