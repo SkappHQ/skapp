011d09c1c9b343ef5c92ec696db97acd112e1cb1
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-02-24 13:00:46 +0530

    feat/556: quick setup

---
+++ common/payload/response/EpQuickSetupProgressResponseDto.java
@@ -0,0 +1,19 @@
+package com.skapp.enterprise.common.payload.response;
+
+import com.skapp.enterprise.common.type.QuickSetupType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class EpQuickSetupProgressResponseDto {
+
+	private double progress;
+
+	private Map<QuickSetupType, Boolean> quickSetupStatus;
+
+}
