ff6e7f336c3dacfb4620215cab7ffadb7cda057d
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2025-03-04 09:30:00 +0530

    feat:272:merge to develop 5

--- common/controller/v1/StripeController.java
+++ common/controller/v1/StripeController.java
@@ -6,6 +6,7 @@ import com.skapp.enterprise.common.payload.request.CreateSubscriptionRequestDto;
 import com.skapp.enterprise.common.payload.request.PaymentMethodRequestDto;
 import com.skapp.enterprise.common.payload.request.PromotionCodeRequestDto;
 import com.skapp.enterprise.common.service.StripeService;
+import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +30,7 @@ public class StripeController {
 
 	@PostMapping("/webhook")
 	public ResponseEntity<Void> handleStripeWebhook(@RequestBody String payload,
-			@RequestHeader("Stripe-Signature") String sigHeader) throws StripeException {
+			@RequestHeader("Stripe-Signature") String sigHeader) throws SignatureVerificationException {
 		stripeService.handleStripeEvent(payload, sigHeader);
 		return ResponseEntity.ok().build();
 	}
