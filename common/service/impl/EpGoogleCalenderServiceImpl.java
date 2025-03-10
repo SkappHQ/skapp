056b7b7a09ae65a8a7169f6abeaf9c35ecdd62f7
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-03-10 17:30:38 +0530

    fix/735: timezone issue in google calendar

--- common/service/impl/EpGoogleCalenderServiceImpl.java
+++ common/service/impl/EpGoogleCalenderServiceImpl.java
@@ -25,8 +25,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.skapp.community.common.constant.CommonMessageConstant;
 import com.skapp.community.common.exception.EntityNotFoundException;
 import com.skapp.community.common.exception.ModuleException;
+import com.skapp.community.common.model.Organization;
 import com.skapp.community.common.model.User;
 import com.skapp.community.common.payload.response.ResponseEntityDto;
+import com.skapp.community.common.repository.OrganizationDao;
 import com.skapp.community.common.repository.UserDao;
 import com.skapp.community.common.service.EncryptionDecryptionService;
 import com.skapp.community.common.service.UserService;
@@ -98,6 +100,7 @@ public class EpGoogleCalenderServiceImpl implements EpGoogleCalenderService {
 	private static final JsonFactory JSON_FACTORY = new GsonFactory();
 
 	private final MessageUtil messageUtil;
+	private final OrganizationDao organizationDao;
 
 	@Value("${encryptDecryptAlgorithm.secret}")
 	private String encryptSecret;
@@ -372,8 +375,15 @@ public class EpGoogleCalenderServiceImpl implements EpGoogleCalenderService {
 	public String createOutOfOfficeEvent(LocalDateTime startDateTime, LocalDateTime endDateTime, String accessToken,
 			String autoDeclineMode, String declineMessage) {
 
-		ZonedDateTime startUtc = startDateTime.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("UTC"));
-		ZonedDateTime endUtc = endDateTime.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("UTC"));
+		String organizationTimeZone = organizationDao.findTopByOrderByOrganizationIdDesc()
+				.map(Organization::getOrganizationTimeZone)
+				.orElse("UTC");
+
+		ZonedDateTime startInOrgTz = startDateTime.atZone(ZoneId.of(organizationTimeZone));
+		ZonedDateTime endInOrgTz = endDateTime.atZone(ZoneId.of(organizationTimeZone));
+
+		ZonedDateTime startUtc = startInOrgTz.withZoneSameInstant(ZoneId.of("UTC"));
+		ZonedDateTime endUtc = endInOrgTz.withZoneSameInstant(ZoneId.of("UTC"));
 
 		String startFormatted = EpDateTimeUtils.formatUtcDateTime(startUtc);
 		String endFormatted = EpDateTimeUtils.formatUtcDateTime(endUtc);
