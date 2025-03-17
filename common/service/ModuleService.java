1bc483e5bb3ea73541eae51fdee85276fe565819
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-03-17 18:14:29 +0530

    fix/835: quick setup module on off problem

--- common/service/ModuleService.java
+++ common/service/ModuleService.java
@@ -1,6 +1,8 @@
 package com.skapp.enterprise.common.service;
 
 import com.skapp.community.common.payload.response.ResponseEntityDto;
+import com.skapp.community.common.type.ModuleType;
+import com.skapp.enterprise.common.model.ModuleConfig;
 import com.skapp.enterprise.common.payload.request.UpdateModulesRequestDto;
 
 public interface ModuleService {
@@ -8,5 +10,7 @@ public interface ModuleService {
 	ResponseEntityDto updateModules(UpdateModulesRequestDto updateModulesRequestDto);
 
 	ResponseEntityDto getActiveModules();
+
+	boolean getCurrentModuleState(ModuleConfig moduleConfig, ModuleType moduleType);
 
 }
