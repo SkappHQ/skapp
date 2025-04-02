c4347d23ae76fea570b25ab771cc68cb7b6135ec
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-04-02 14:20:14 +0530

    hotfix: make timeline add for free also

--- leaveplanner/service/impl/EpLeaveEntitlementServiceImpl.java
+++ leaveplanner/service/impl/EpLeaveEntitlementServiceImpl.java
@@ -17,8 +17,6 @@ import com.skapp.community.leaveplanner.service.impl.LeaveEntitlementServiceImpl
 import com.skapp.community.peopleplanner.mapper.PeopleMapper;
 import com.skapp.community.peopleplanner.model.Employee;
 import com.skapp.community.peopleplanner.repository.EmployeeDao;
-import com.skapp.enterprise.common.type.Tier;
-import com.skapp.enterprise.people.service.EpUserService;
 import com.skapp.enterprise.people.service.impl.EpEmployeeTimelineServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
@@ -34,54 +32,39 @@ public class EpLeaveEntitlementServiceImpl extends LeaveEntitlementServiceImpl {
 
 	private final EpEmployeeTimelineServiceImpl epEmployeeTimelineServiceImpl;
 
-	private final EpUserService epUserService;
-
 	public EpLeaveEntitlementServiceImpl(MessageUtil messageUtil, EmployeeDao employeeDao,
 			LeaveCycleService leaveCycleService, LeaveTypeDao leaveTypeDao, LeaveEntitlementDao leaveEntitlementDao,
 			LeaveMapper leaveMapper, PeopleMapper peopleMapper, CarryForwardInfoDao carryForwardInfoDao,
 			PageTransformer pageTransformer, PlatformTransactionManager transactionManager, UserDao userDao,
 			UserService userService, LeaveEmailService leaveEmailService,
 			LeaveNotificationService leaveNotificationService, BulkContextService bulkContextService,
-			EpEmployeeTimelineServiceImpl epEmployeeTimelineServiceImpl, EpUserService epUserService) {
+			EpEmployeeTimelineServiceImpl epEmployeeTimelineServiceImpl) {
 		super(messageUtil, employeeDao, leaveCycleService, leaveTypeDao, leaveEntitlementDao, leaveMapper, peopleMapper,
 				carryForwardInfoDao, pageTransformer, transactionManager, userDao, userService, leaveEmailService,
 				leaveNotificationService, bulkContextService);
 		this.epEmployeeTimelineServiceImpl = epEmployeeTimelineServiceImpl;
-		this.epUserService = epUserService;
 	}
 
 	@Override
 	protected void addCustomLeaveEntitlementsTimeLineRecords(Employee employee, LeaveEntitlement leaveEntitlement) {
-		Tier currentUserTier = epUserService.getCurrentUserTier();
-		if (currentUserTier == Tier.PRO) {
-			epEmployeeTimelineServiceImpl.addCustomLeaveEntitlementsTimeLineRecords(employee, leaveEntitlement);
-		}
+		epEmployeeTimelineServiceImpl.addCustomLeaveEntitlementsTimeLineRecords(employee, leaveEntitlement);
 	}
 
 	@Override
 	protected void addBulkLeaveEntitlementsTimeLineRecords(Employee employee, List<LeaveEntitlement> entitlements) {
-		Tier currentUserTier = epUserService.getCurrentUserTier();
-		if (currentUserTier == Tier.PRO) {
-			epEmployeeTimelineServiceImpl.addBulkLeaveEntitlementsTimeLineRecords(employee, entitlements, false);
-		}
+		epEmployeeTimelineServiceImpl.addBulkLeaveEntitlementsTimeLineRecords(employee, entitlements, false);
 	}
 
 	@Override
 	protected void addDeletedLeaveEntitlementsTimeLineRecords(Employee employee, String oldHistoryRecord) {
-		Tier currentUserTier = epUserService.getCurrentUserTier();
-		if (currentUserTier == Tier.PRO) {
-			epEmployeeTimelineServiceImpl.addDeletedLeaveEntitlementsTimeLineRecords(employee, oldHistoryRecord);
-		}
+		epEmployeeTimelineServiceImpl.addDeletedLeaveEntitlementsTimeLineRecords(employee, oldHistoryRecord);
 	}
 
 	@Override
 	protected void addUpdatedLeaveEntitlementsTimeLineRecords(Employee employee, String oldHistoryRecord,
 			String newHistoryRecord, boolean isCustom) {
-		Tier currentUserTier = epUserService.getCurrentUserTier();
-		if (currentUserTier == Tier.PRO) {
-			epEmployeeTimelineServiceImpl.addUpdatedLeaveEntitlementsTimeLineRecords(employee, oldHistoryRecord,
-					newHistoryRecord, isCustom);
-		}
+		epEmployeeTimelineServiceImpl.addUpdatedLeaveEntitlementsTimeLineRecords(employee, oldHistoryRecord,
+				newHistoryRecord, isCustom);
 	}
 
 }
