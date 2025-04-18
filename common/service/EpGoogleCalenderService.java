6e0d5c90eb1c1a45bcc46706c7755566530a3f79
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-04-18 14:46:07 +0530

    fix: variable name issues

--- common/service/EpGoogleCalenderService.java
+++ common/service/EpGoogleCalenderService.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 public interface EpGoogleCalenderService {
 
-	String connectGoogleCalendar(EpGoogleAuthRedirectDto epGoogleCalendarauthRedirectDto);
+	String connectGoogleCalendar(EpGoogleAuthRedirectDto epGoogleAuthRedirectDto);
 
 	ResponseEntityDto saveGoogleCalendarConfig(EpGoogleAuthRedirectDto epGoogleAuthRedirectDto);
 
