74c960d26efff8f97de59a87462842fe81ea9252
HarinduMalinka <harindu.desilva@rootcode.io>
2025-04-24 16:24:58 +0530

    conflict fix

--- esignature/constant/EsignMessageConstant.java
+++ esignature/constant/EsignMessageConstant.java
@@ -105,7 +105,6 @@ public enum EsignMessageConstant implements MessageConstant {
 	ESIGN_ERROR_DOCUMENT_ACCESS_LINK_ALREADY_RESEND("ep.esign.error.document.access.already.resend"),
 	ESIGN_ERROR_ENVELOPE_UUID_CREATION_FAIL("ep.esign.error.envelope.uuid.creation.fail"),
 	ESIGN_ERROR_USER_ALREADY_OWNER_OF_ENVELOPE("ep.esign.error.user.already.owner.of.envelope"),
-	ESIGN_ERROR_ENVELOPE_UUID_CREATION_FAIL("ep.esign.error.envelope.uuid.creation.fail"),
 	ESIGN_ERROR_RECIPIENT_ALREADY_DECLINED_ENVELOP("ep.esign.error.recipient.already-declined-envelope"),
 	ESIGN_ERROR_RECIPIENT_ALREADY_APPROVED("ep.esign.error.recipient.already-approved"),
 	ESIGN_ERROR_ENVELOPE_ALREADY_DECLINED("ep.esign.error.envelope.already-declined"),
