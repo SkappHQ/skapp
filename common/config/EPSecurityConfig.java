6e753c06fbfcccc6c7bcdb769f296148eb0ded09
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-05-05 15:59:05 +0530

    feat/1480: microsoft sso implementation

--- common/config/EPSecurityConfig.java
+++ common/config/EPSecurityConfig.java
@@ -86,7 +86,9 @@ public class EPSecurityConfig {
 						"/v1/google-calendar/redirect", "/v1/validate/email", "/v1/ep/stripe/webhook",
 						"/v2/ep/auth/sso/google/auth-url", "/v2/ep/auth/sso/google/redirect",
 						"/v2/ep/auth/signin/sso/google", "/v2/ep/auth/signup/super-admin/sso/google",
-						"/v1/ep/auth/code-challenge/verify")
+						"/v1/ep/auth/code-challenge/verify", "/v2/ep/auth/sso/microsoft/auth-url",
+						"/v2/ep/auth/sso/microsoft/redirect", "/v2/ep/auth/signup/super-admin/sso/microsoft",
+						"/v2/ep/auth/signin/sso/microsoft")
 				.permitAll()
 				.requestMatchers("/v1/reset-database")
 				.permitAll()
--- common/service/v2/EpAuthServiceV2.java
+++ common/service/v2/EpAuthServiceV2.java
@@ -3,6 +3,10 @@ package com.skapp.enterprise.common.service.v2;
 import com.skapp.community.common.payload.response.ResponseEntityDto;
 import com.skapp.enterprise.common.payload.request.EpGoogleAuthRedirectDto;
 import com.skapp.enterprise.common.payload.request.EpGoogleConsentUrlDto;
+import com.skapp.enterprise.common.payload.request.EpMicrosoftAuthRedirectDto;
+import com.skapp.enterprise.common.payload.request.EpMicrosoftConsentUrlDto;
+import com.skapp.enterprise.common.payload.v2.request.EpSignInMicrosoftDataDto;
+import com.skapp.enterprise.common.payload.v2.request.EpSignUpMicrosoftDataDto;
 import com.skapp.enterprise.common.payload.v2.request.EpSignInGoogleDataDto;
 import com.skapp.enterprise.common.payload.v2.request.EpSignUpGoogleDataDto;
 import jakarta.validation.Valid;
@@ -16,5 +20,13 @@ public interface EpAuthServiceV2 {
 	String ssoGoogleSignInRedirect(@Valid EpGoogleAuthRedirectDto epGoogleAuthRedirectDto);
 
 	ResponseEntityDto getGoogleAuthUrl(@Valid EpGoogleConsentUrlDto epGoogleConsentUrlDto);
+
+	ResponseEntityDto getMicrosoftAuthUrl(@Valid EpMicrosoftConsentUrlDto epMicrosoftConsentUrlDto);
+
+	String ssoMicrosoftSignInRedirect(@Valid EpMicrosoftAuthRedirectDto epMicrosoftAuthRedirectDto);
+
+	ResponseEntityDto ssoMicrosoftSignUp(@Valid EpSignUpMicrosoftDataDto epSignUpMicrosoftDataDto);
+
+	ResponseEntityDto ssoMicrosoftSignIn(@Valid EpSignInMicrosoftDataDto epSignUpMicrosoftDataDto);
 
 }
