a7b6a3663583e3072899e1d8ec2bbf54e93cfc32
HarinduMalinka <harindu.desilva@rootcode.io>
2025-05-08 16:08:11 +0530

    fix space issue in contacts create and edit

--- esignature/payload/request/ExternalPatchUserDto.java
+++ esignature/payload/request/ExternalPatchUserDto.java
@@ -1,5 +1,6 @@
 package com.skapp.enterprise.esignature.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class ExternalPatchUserDto {
 
+	@NotBlank(message = "firstName cannot be blank")
 	private String firstName;
 
 	private String lastName;
