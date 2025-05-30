1b7daac5e2a460869c9a52963267b3a78c49e265
HarinduMalinka <harindu.desilva@rootcode.io>
2025-05-30 12:06:54 +0530

    added decline reason

--- esignature/service/impl/RecipientServiceImpl.java
+++ esignature/service/impl/RecipientServiceImpl.java
@@ -467,8 +467,9 @@ public class RecipientServiceImpl implements RecipientService {
 			String senderEmail = envelope.getOwner().getEmail();
 
 			if (envelope.getStatus() == EnvelopeStatus.DECLINED) {
-				declinedBy = obtainEnvelopeDeclinedBy(recipientList);
-				voidOrDeclinedReason = envelope.getVoidReason();
+				Recipient declinedRecipient = obtainEnvelopeDeclinedBy(recipientList);
+				declinedBy = declinedRecipient.getAddressBook().getName();
+				voidOrDeclinedReason = declinedRecipient.getDeclineReason();
 				title = EsignEmailTitleConstant.ESIGN_ENVELOPE_DECLINED_EMAIL_TITLE;
 			}
 			else if (envelope.getStatus() == EnvelopeStatus.VOIDED) {
@@ -694,16 +695,13 @@ public class RecipientServiceImpl implements RecipientService {
 	 * @return This method is used to find who declined the envelope in order to mention
 	 * that in the email
 	 */
-	private String obtainEnvelopeDeclinedBy(List<Recipient> recipients) {
+	private Recipient obtainEnvelopeDeclinedBy(List<Recipient> recipients) {
 
 		Optional<Recipient> declinedRecipient = recipients.stream()
 			.filter(recpt -> recpt.getStatus() == RecipientStatus.DECLINED)
 			.findFirst();
 
-		if (declinedRecipient.isPresent()) {
-			return declinedRecipient.get().getAddressBook().getName();
-		}
-		return null;
+		return declinedRecipient.orElse(null);
 	}
 
 	private RecipientUpdateDto initializerecipientDtoData(RecipientStatus recipientStatus, String reminderBatchId,
