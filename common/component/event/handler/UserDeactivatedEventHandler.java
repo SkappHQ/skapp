a774d6a2ef50a3580d05bc81dfa22f1151b9b2eb
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2025-06-05 13:48:28 +0530

    Add addresbook event to trigger deactivate users when user deactivate happen from Skapp flow

--- common/component/event/handler/UserDeactivatedEventHandler.java
+++ common/component/event/handler/UserDeactivatedEventHandler.java
@@ -2,10 +2,15 @@ package com.skapp.enterprise.common.component.event.handler;
 
 import com.skapp.community.common.model.User;
 import com.skapp.community.common.util.event.UserDeactivatedEvent;
+import com.skapp.community.common.util.event.UsersDeactivatedEvent;
+import com.skapp.enterprise.esignature.model.AddressBook;
 import com.skapp.enterprise.esignature.repository.AddressBookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UserDeactivatedEventHandler {
@@ -24,6 +29,20 @@ public class UserDeactivatedEventHandler {
 			addressBook.setIsActive(false);
 			addressBookDao.save(addressBook);
 		});
+	}
+
+	@EventListener
+	public void handleUsersDeactivation(UsersDeactivatedEvent event) {
+		List<AddressBook> deactivateAddressBookUsers = new ArrayList<>();
+		for (User user : event.getUsers()) {
+			addressBookDao.findByInternalUser(user).ifPresent(addressBook -> {
+				addressBook.setIsActive(false);
+				deactivateAddressBookUsers.add(addressBook);
+			});
+		}
+		if (!deactivateAddressBookUsers.isEmpty()) {
+			addressBookDao.saveAll(deactivateAddressBookUsers);
+		}
 	}
 
 }
