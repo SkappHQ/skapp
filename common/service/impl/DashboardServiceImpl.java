48851c6ae25d93b4733482f1e8fdca53d1fbeadd
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-06-19 17:01:22 +0530

    fix: simplify new organization email method by removing unnecessary parameters

--- common/service/impl/DashboardServiceImpl.java
+++ common/service/impl/DashboardServiceImpl.java
@@ -1,10 +1,10 @@
 package com.skapp.enterprise.common.service.impl;
 
-import com.skapp.community.common.model.Organization;
-import com.skapp.community.common.repository.OrganizationDao;
 import com.skapp.community.peopleplanner.repository.EmployeeDao;
 import com.skapp.community.peopleplanner.type.AccountStatus;
+import com.skapp.enterprise.common.model.EpOrganization;
 import com.skapp.enterprise.common.payload.DashboardEmailOrganizationDetailsDto;
+import com.skapp.enterprise.common.repository.EpOrganizationDao;
 import com.skapp.enterprise.common.service.DashboardService;
 import com.skapp.enterprise.common.util.EpDateTimeUtils;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
-import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -20,20 +19,18 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class DashboardServiceImpl implements DashboardService {
 
-	private final OrganizationDao organizationDao;
+	private final EpOrganizationDao epOrganizationDao;
 
 	private final EmployeeDao employeeDao;
 
 	@Override
 	public DashboardEmailOrganizationDetailsDto getDashboardEmailOrganizationDetails(String superAdminEmail) {
-		Optional<Organization> optionalOrganization = organizationDao.findTopByOrderByOrganizationIdDesc();
-		String companyName = optionalOrganization.map(Organization::getOrganizationName).orElse(null);
-
+		EpOrganization organization = epOrganizationDao.findTopByOrderByOrganizationIdDesc();
+		String companyName = organization.getOrganizationName();
+		String contactNumber = organization.getContactNo();
 		String formattedCurrentTime = EpDateTimeUtils.DATE_TIME_FORMATTER.format(ZonedDateTime.now());
 
 		long userCount = employeeDao.countByAccountStatusIn(Set.of(AccountStatus.ACTIVE, AccountStatus.PENDING));
-
-		String contactNumber = "";
 
 		DashboardEmailOrganizationDetailsDto dashboardEmailOrganizationDetailsDto = new DashboardEmailOrganizationDetailsDto();
 		dashboardEmailOrganizationDetailsDto.setCompanyName(companyName);
