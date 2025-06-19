51d0c0758e7038661a8b3474901f67f9dd93629b
Shakila Kamalasena <shakila.kamalasena@rootcode.io>
2025-06-19 14:21:09 +0530

    Add DrawerCloseIcon and DrawerOpenIcon components with corresponding updates to Icon mapping

--- frontend/src/community/common/components/atoms/Icon/Icon.tsx
+++ frontend/src/community/common/components/atoms/Icon/Icon.tsx
@@ -43,6 +43,8 @@ import DocumentsIcon from "~community/common/assets/Icons/DocumentsIcon";
 import DottedClockIcon from "~community/common/assets/Icons/DottedClockIcon";
 import DownloadIcon from "~community/common/assets/Icons/DownloadIcon";
 import DragIcon from "~community/common/assets/Icons/DragIcon";
+import DrawerCloseIcon from "~community/common/assets/Icons/DrawerCloseIcon";
+import DrawerOpenIcon from "~community/common/assets/Icons/DrawerOpenIcon";
 import DropDownArrowIcon from "~community/common/assets/Icons/DropDownArrowIcon";
 import DropDownArrow from "~community/common/assets/Icons/DropdownArrow";
 import EditIcon from "~community/common/assets/Icons/EditIcon";
@@ -275,7 +277,9 @@ export const IconMapping: Partial<
   [IconName.CRM_MODULE_ICON]: CRMModuleIcon,
   [IconName.HELP_DESK_MODULE_ICON]: HelpDeskModuleIcon,
   [IconName.PROJECT_MANAGEMENT_MODULE_ICON]: ProjectManagementModuleIcon,
-  [IconName.BLOCKED_ICON]: BlockedIcon
+  [IconName.BLOCKED_ICON]: BlockedIcon,
+  [IconName.DRAWER_CLOSE_ICON]: DrawerCloseIcon,
+  [IconName.DRAWER_OPEN_ICON]: DrawerOpenIcon
   // Add more icons here as needed
 };
 
