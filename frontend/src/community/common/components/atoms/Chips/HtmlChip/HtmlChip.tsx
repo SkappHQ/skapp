c6e5b7bc12008101ea68a85ec4803f26214e6780
Shakya Karunathilake <108653862+shakyaatrootcode@users.noreply.github.com>
2025-06-19 10:41:40 +0530

    feat: add HtmlChip component and utility functions for attendance headers

---
+++ frontend/src/community/common/components/atoms/Chips/HtmlChip/HtmlChip.tsx
@@ -0,0 +1,64 @@
+import { useTheme } from "@mui/material";
+import React from "react";
+
+import { getEmoji } from "~community/common/utils/commonUtil";
+
+const HtmlChip = ({
+  className,
+  emoji,
+  text,
+  customStyles = {
+    text: {},
+    emoji: {}
+  }
+}: {
+  className?: string;
+  emoji?: string;
+  text: string;
+  customStyles?: {
+    text?: React.CSSProperties;
+    emoji?: React.CSSProperties;
+  };
+}) => {
+  const theme = useTheme();
+
+  return (
+    <span
+      style={{
+        fontSize: "0.625rem",
+        lineHeight: "0.9375rem",
+        padding: "0.1875rem 0.1875rem",
+        borderRadius: "6.25rem",
+        backgroundColor: theme.palette.common.white,
+        border: `0.0625rem solid ${theme.palette.grey[500]}`,
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        maxWidth: "100%",
+        display: "inline-block",
+        verticalAlign: "middle",
+        ...customStyles.text
+      }}
+    >
+      {emoji ? (
+        <span
+          className={className}
+          style={{
+            marginRight: "0.25rem",
+            borderRadius: "100%",
+            fontSize: "0.5rem",
+            padding: "0.125rem",
+            ...customStyles.emoji
+          }}
+        >
+          {getEmoji(emoji)}
+        </span>
+      ) : (
+        <></>
+      )}
+      {text}
+    </span>
+  );
+};
+
+export default HtmlChip;
