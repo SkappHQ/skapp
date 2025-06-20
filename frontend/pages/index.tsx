a26acbcc429819757e9a61218df43e5659ed5187
hasalarootcode <hasala.abhilasha@rootcode.io>
2025-06-20 11:38:09 +0530

    fix: root url redirection issue

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,65 @@
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
+import { organizationCreateEndpoints } from "~community/common/api/utils/ApiEndpoints";
 import { appModes } from "~community/common/constants/configs";
+import { HTTP_OK } from "~community/common/constants/httpStatusCodes";
 import ROUTES from "~community/common/constants/routes";
-import { useRedirectHandler } from "~community/common/utils/hooks/useRedirectHandler";
+import authFetch from "~community/common/utils/axiosInterceptor";
+
+export async function getServerSideProps() {
+  const isEnterprise = process.env.NEXT_PUBLIC_MODE === appModes.ENTERPRISE;
+
+  try {
+    if (!isEnterprise) {
+      const response = await authFetch.get(
+        organizationCreateEndpoints.CHECK_ORG_SETUP_STATUS
+      );
+
+      if (response.status === HTTP_OK) {
+        const orgSetupStatus = await response.data;
+
+        if (orgSetupStatus?.results[0]) {
+          if (!orgSetupStatus?.results[0]?.isSignUpCompleted) {
+            return {
+              redirect: {
+                destination: ROUTES.AUTH.SIGNUP,
+                permanent: false
+              }
+            };
+          } else if (
+            !orgSetupStatus?.results[0]?.isOrganizationSetupCompleted
+          ) {
+            return {
+              redirect: {
+                destination: ROUTES.ORGANIZATION.SETUP,
+                permanent: false
+              }
+            };
+          }
+        }
+      }
+    }
+  } catch (error) {
+    return {
+      redirect: {
+        destination: ROUTES.DASHBOARD.BASE,
+        permanent: false
+      }
+    };
+  }
+
+  if (isEnterprise) {
+    return {
+      redirect: {
+        destination: ROUTES.DASHBOARD.BASE,
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+}
 
 export default function Index() {
-  const isEnterprise = process.env.NEXT_PUBLIC_MODE === appModes.ENTERPRISE;
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isEnterprise) {
-      router.replace(ROUTES.DASHBOARD.BASE);
-    }
-  }, []);
-
-  useRedirectHandler({ isSignInPage: false });
-
   return null;
 }
