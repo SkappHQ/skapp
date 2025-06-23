bfe3b5ae85a68d933758bd0ad4e973021993bcd4
Shakila Kamalasena <shakila.kamalasena@rootcode.io>
2025-06-23 12:28:03 +0530

    update(Drawer): change width property for small breakpoint

--- frontend/src/community/common/components/organisms/Drawer/StyledDrawer.tsx
+++ frontend/src/community/common/components/organisms/Drawer/StyledDrawer.tsx
@@ -18,7 +18,7 @@ export const StyledDrawer = styled(Drawer)(({ theme, open }) => ({
         "0px 4px 10px 0px rgba(0, 0, 0, 0.06), 0px 40px 24px 0px rgba(0, 0, 0, 0.03), 0px 71px 28px 0px rgba(0, 0, 0, 0.01), 0px 110px 31px 0px rgba(0, 0, 0, 0)"
     },
     [theme.breakpoints.up("sm")]: {
-      width: open ? "16.75rem" : "3rem"
+      width: open ? "16.75rem" : "0rem"
     },
     [theme.breakpoints.up("lg")]: {
       width: open ? "16.75rem" : "3rem",
