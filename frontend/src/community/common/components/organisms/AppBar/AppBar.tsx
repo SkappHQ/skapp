8f4da337c8cc7b1298ef0ff5f7cc8fdead183a8c
Shakila Kamalasena <shakila.kamalasena@rootcode.io>
2025-06-27 16:30:47 +0530

    fix(AppBar): reduce height of user info panel skeleton

--- frontend/src/community/common/components/organisms/AppBar/AppBar.tsx
+++ frontend/src/community/common/components/organisms/AppBar/AppBar.tsx
@@ -149,7 +149,7 @@ const AppBar = () => {
           ) : (
             <Skeleton
               variant="rounded"
-              height="4.5rem"
+              height="3.75rem"
               width="7.5rem"
               sx={classes.userInfoPanelWrapper}
               animation={"wave"}
