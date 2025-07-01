6ef184c1d38be4300f1f3224c28d7f134b4099a5
Shakila Kamalasena <shakila.kamalasena@rootcode.io>
2025-07-01 19:37:08 +0530

    update(Drawer): adjust width and padding

--- frontend/src/community/common/components/organisms/Drawer/StyledDrawer.tsx
+++ frontend/src/community/common/components/organisms/Drawer/StyledDrawer.tsx
@@ -6,7 +6,7 @@ import { ANIMATION_DURATION } from "./styles";
 export const StyledDrawer = styled(Drawer)(({ theme, open }) => ({
   "&.MuiDrawer-docked": {
     [theme.breakpoints.up("lg")]: {
-      width: open ? "16.75rem" : "3rem",
+      width: open ? "17.75rem" : "3rem",
       height: "100dvh",
       transition: `width ${ANIMATION_DURATION} ease`
     }
@@ -21,10 +21,10 @@ export const StyledDrawer = styled(Drawer)(({ theme, open }) => ({
         "0px 4px 10px 0px rgba(0, 0, 0, 0.06), 0px 40px 24px 0px rgba(0, 0, 0, 0.03), 0px 71px 28px 0px rgba(0, 0, 0, 0.01), 0px 110px 31px 0px rgba(0, 0, 0, 0)"
     },
     [theme.breakpoints.up("sm")]: {
-      width: open ? "16.75rem" : "0rem"
+      width: open ? "17.75rem" : "0rem"
     },
     [theme.breakpoints.up("lg")]: {
-      width: open ? "16.75rem" : "3rem",
+      width: open ? "17.75rem" : "3rem",
       "&:hover .MuiIconButton-root": {
         opacity: 1,
         visibility: "visible"
