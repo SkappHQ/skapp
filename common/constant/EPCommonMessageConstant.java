30179360930e9a9e2e71cd2a1415554aec0ceb1b
Akila Silva <akila.silva@rootcode.io>
2025-07-08 17:41:14 +0530

    conflicts resolving

--- common/constant/EPCommonMessageConstant.java
+++ common/constant/EPCommonMessageConstant.java
@@ -158,7 +158,6 @@ public enum EPCommonMessageConstant implements MessageConstant {
 	EP_COMMON_ERROR_CLOUD_FRONT_SIGNED_COOKIES_GENERATION_FAILED(
 			"ep.common.error.cloud-front-signed-cookies-generation-failed"),
 	EP_COMMON_ERROR_CLOUD_FRONT_FAILED_TO_PARSE_PRIVATE_KEY("ep.common.error.cloud-front-failed-to-parse-private-key"),
-	EP_COMMON_ERROR_INVALID_S3_FOLDER_PATH("ep.common.error.invalid-s3-folder-path"),
 	EP_COMMON_ERROR_MIGRATION_SERVICE_UNAVAILABLE("ep.common.error.migration-service-unavailable"),;
 
 	private final String messageKey;
