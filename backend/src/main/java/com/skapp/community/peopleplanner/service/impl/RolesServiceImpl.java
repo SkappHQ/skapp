e5346ab267ee2f3f6eb33e86743ec29c2fda0afa
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-07-25 10:49:16 +0530

    feat: add project management role to default employee roles

--- backend/src/main/java/com/skapp/community/peopleplanner/service/impl/RolesServiceImpl.java
+++ backend/src/main/java/com/skapp/community/peopleplanner/service/impl/RolesServiceImpl.java
@@ -263,6 +263,7 @@ public class RolesServiceImpl implements RolesService {
 		defaultEmployeeRoles.setLeaveRole(Role.LEAVE_EMPLOYEE);
 		defaultEmployeeRoles.setAttendanceRole(Role.ATTENDANCE_EMPLOYEE);
 		defaultEmployeeRoles.setEsignRole(Role.ESIGN_EMPLOYEE);
+		defaultEmployeeRoles.setEsignRole(Role.PM_EMPLOYEE);
 		return defaultEmployeeRoles;
 	}
 
