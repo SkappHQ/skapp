2c56ce918a27fab4d850112457fd308b92f39ba8
Devni Attigala <devni.attigala@rootcode.io>
2025-07-29 09:14:00 +0530

    feat: Fix the mapper to correctly map teamObjective.id to teamObjectiveId

--- backend/src/main/java/com/skapp/community/okrplanner/mapper/TeamObjectiveMapper.java
+++ backend/src/main/java/com/skapp/community/okrplanner/mapper/TeamObjectiveMapper.java
@@ -3,11 +3,15 @@ package com.skapp.community.okrplanner.mapper;
 import com.skapp.community.okrplanner.model.TeamObjective;
 import com.skapp.community.okrplanner.payload.response.TeamObjectiveResponseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TeamObjectiveMapper {
+
+    @Mapping(target = "teamObjectiveId", source = "id")
+    TeamObjectiveResponseDto teamObjectiveToTeamObjectiveResponseDto(TeamObjective teamObjective);
 
     List<TeamObjectiveResponseDto> teamObjectivesToTeamObjectiveResponseDto(List<TeamObjective> teamObjective);
 }
