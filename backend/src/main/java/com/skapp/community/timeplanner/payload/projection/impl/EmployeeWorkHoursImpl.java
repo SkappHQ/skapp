90b4f9bc93ff39b201069599e1e2b726a6a56814
anupama-vm <anupama.morapitiya@rootcode.io>
2025-08-11 20:47:50 +0530

    feat: refactor EmployeeWorkHoursImpl to use Lombok for reduced boilerplate code

--- backend/src/main/java/com/skapp/community/timeplanner/payload/projection/impl/EmployeeWorkHoursImpl.java
+++ backend/src/main/java/com/skapp/community/timeplanner/payload/projection/impl/EmployeeWorkHoursImpl.java
@@ -1,25 +1,16 @@
 package com.skapp.community.timeplanner.payload.projection.impl;
 
 import com.skapp.community.timeplanner.payload.projection.EmployeeWorkHours;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class EmployeeWorkHoursImpl implements EmployeeWorkHours {
     private final LocalDate date;
     private final Double workedHours;
-
-    public EmployeeWorkHoursImpl(LocalDate date, Double workedHours) {
-        this.date = date;
-        this.workedHours = workedHours;
-    }
-
-    @Override
-    public LocalDate getDate() {
-        return date;
-    }
-
-    @Override
-    public Double getWorkedHours() {
-        return workedHours;
-    }
 }
