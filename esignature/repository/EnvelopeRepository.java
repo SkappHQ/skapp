0e25498f22a9eee04c338bd3cd0ffb0b54bff2f6
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2025-08-25 21:47:54 +0530

    2541-implement current user next envelope list fetch flow with expire date sort order

--- esignature/repository/EnvelopeRepository.java
+++ esignature/repository/EnvelopeRepository.java
@@ -3,6 +3,7 @@ package com.skapp.enterprise.esignature.repository;
 import com.skapp.enterprise.esignature.model.Envelope;
 import com.skapp.enterprise.esignature.payload.request.EnvelopeInboxFilterDto;
 import com.skapp.enterprise.esignature.payload.request.EnvelopeSentFilterDto;
+import com.skapp.enterprise.esignature.repository.projection.EnvelopeNextData;
 import com.skapp.enterprise.esignature.type.EnvelopeStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Repository;
@@ -15,6 +16,8 @@ public interface EnvelopeRepository {
 	long countNeedToSignEnvelopes(Long currentUserId);
 
 	Page<Envelope> getAllUserEnvelopes(Long currentUserId, EnvelopeInboxFilterDto envelopeInboxFilterDto);
+
+	Page<EnvelopeNextData> getCurrentUserEnvelopesByExpireDate(Long currentUserId, int page, int size);
 
 	Page<Envelope> getAllSentEnvelopes(Long currentUserId, EnvelopeSentFilterDto envelopeSentFilterDto,
 			boolean isAllSentEnvelopes);
