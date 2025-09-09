507ad7f6f93f62a0c684d7fe95d90036a502c35b
anupama-vm <anupama.morapitiya@rootcode.io>
2025-09-09 18:20:36 +0530

    fix: correct invoice endpoint URL mapping in InvoiceController

--- invoice/controller/v1/InvoiceController.java
+++ invoice/controller/v1/InvoiceController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("v1/ep/invoice")
+@RequestMapping("/v1/ep/invoice")
 public class InvoiceController {
 
 	private final InvoiceService invoiceService;
