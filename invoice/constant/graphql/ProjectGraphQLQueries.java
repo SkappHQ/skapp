92389a63e7a2289a77d730ebc3499bcaf2bf17b9
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2025-09-18 09:15:25 +0530

    GraphQL queriy formatted.

--- invoice/constant/graphql/ProjectGraphQLQueries.java
+++ invoice/constant/graphql/ProjectGraphQLQueries.java
@@ -2,14 +2,7 @@ package com.skapp.enterprise.invoice.constant.graphql;
 
 public class ProjectGraphQLQueries {
 
-	public static final String INTERNAL_PROJECTS = """
-			query InternalProjects {
-									internalProjects {
-													id
-													key
-													name
-									}
-					}
-				\s""";
+	public static final String INTERNAL_PROJECTS = "query InternalProjects { " + "  internalProjects { " + "    id "
+			+ "    key " + "    name " + "  } " + "}";
 
 }
