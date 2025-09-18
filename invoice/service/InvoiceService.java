ebd4f6f048c62e7f6a5360cac43c80417c9d3059
anupama-vm <anupama.morapitiya@rootcode.io>
2025-09-18 15:23:41 +0530

    feat: add success and error messages for email reminders in InvoiceMessageConstant

--- invoice/service/InvoiceService.java
+++ invoice/service/InvoiceService.java
@@ -3,6 +3,7 @@ package com.skapp.enterprise.invoice.service;
 import com.skapp.community.common.payload.response.ResponseEntityDto;
 import com.skapp.enterprise.invoice.payload.request.InvoiceFilterRequestDto;
 import com.skapp.enterprise.invoice.payload.request.invoice.CreateInvoiceRequestDto;
+import com.skapp.enterprise.invoice.payload.request.invoice.InvoiceStatusUpdateRequestDto;
 
 public interface InvoiceService {
 
