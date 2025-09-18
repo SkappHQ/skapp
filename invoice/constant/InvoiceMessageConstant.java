58de56731686ac1bcb193877cb645f791f17cb5e
anupama-vm <anupama.morapitiya@rootcode.io>
2025-09-18 16:41:03 +0530

    feat: add new invoice error message constants for project fetching

--- invoice/constant/InvoiceMessageConstant.java
+++ invoice/constant/InvoiceMessageConstant.java
@@ -80,7 +80,10 @@ public enum InvoiceMessageConstant implements MessageConstant {
 
 	INVOICE_ERROR_CUSTOMER_DOCUMENT_NAME_REQUIRED("invoice.error.customer.document.name.required"),
 	INVOICE_ERROR_CUSTOMER_DOCUMENT_URL_REQUIRED("invoice.error.customer.document.url.required"),
-	INVOICE_ERROR_CUSTOMER_DOCUMENT_FILTER_INVALID("invoice.error.customer.document.filter.invalid");
+	INVOICE_ERROR_CUSTOMER_DOCUMENT_FILTER_INVALID("invoice.error.customer.document.filter.invalid"),
+
+	INVOICE_ERROR_FETCHING_PROJECTS("ep.invoice.error.fetching.projects"),
+	INVOICE_ERROR_FETCHING_PROJECTS_FROM_SOURCE("ep.invoice.error.fetching.projects.from.source");
 
 	private final String messageKey;
 
