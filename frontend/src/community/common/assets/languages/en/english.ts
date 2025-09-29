01beba05491559651a632219161b9dc918185948
Shakila Kamalasena <shakila.kamalasena@rootcode.io>
2025-09-29 16:07:27 +0530

    fix: update subproject commits and restore ARIA labels in English language module

--- frontend/src/community/common/assets/languages/en/english.ts
+++ frontend/src/community/common/assets/languages/en/english.ts
@@ -1,3 +1,8 @@
+import attendanceAria from "~community/common/assets/aria-labels/en/attendance.json";
+import commonAria from "~community/common/assets/aria-labels/en/common.json";
+import leaveAria from "~community/common/assets/aria-labels/en/leave.json";
+import onboardingAria from "~community/common/assets/aria-labels/en/onboarding.json";
+import peopleAria from "~community/common/assets/aria-labels/en/people.json";
 import attendanceModule from "~community/common/assets/languages/en/attendanceModule.json";
 import common from "~community/common/assets/languages/en/common.json";
 import configurations from "~community/common/assets/languages/en/configurations.json";
@@ -11,6 +16,11 @@ export const english = {
   configurations,
   peopleModule,
   leaveModule,
-  attendanceModule
+  attendanceModule,
+  onboardingAria,
+  attendanceAria,
+  commonAria,
+  leaveAria,
+  peopleAria
   // add other modules here
 };
