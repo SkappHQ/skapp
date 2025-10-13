64da4dae9d0eb092ddcd07bccc92563cd0571278
menura-rootcode <menura.wijesekara@rootcode.io>
2025-10-13 18:48:36 +0530

    refactor: simplify country code retrieval logic in useGetCountryCodeByName hook

--- frontend/src/community/people/hooks/useGetCountryCodeByName.tsx
+++ frontend/src/community/people/hooks/useGetCountryCodeByName.tsx
@@ -1,12 +1,9 @@
 import { countryCodeList } from "../data/countryCodes";
 
 const useGetCountryCodeByName = (country: string) => {
-  if (country) {
-    const matchedCountry = countryCodeList.find(
-      (item) => item.name.toLowerCase() === country.toLowerCase()
-    );
-    return matchedCountry ? matchedCountry.code : "";
-  }
-  return "";
+  const matchedCountry = countryCodeList.find(
+    (item) => country && item.name.toLowerCase() === country.toLowerCase()
+  );
+  return matchedCountry ? matchedCountry.code : "";
 };
 export default useGetCountryCodeByName;
