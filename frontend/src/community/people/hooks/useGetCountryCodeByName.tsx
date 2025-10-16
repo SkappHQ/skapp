ff9b88b97b8014738fcea5a2dbbe021327c394b4
menura-rootcode <menura.wijesekara@rootcode.io>
2025-10-16 12:13:45 +0530

    refactor: remove unused useGetCountryCodeByName hook

--- frontend/src/community/people/hooks/useGetCountryCodeByName.tsx
+++
@@ -1,9 +0,0 @@
-import { countryCodeList } from "../data/countryCodes";
-
-const useGetCountryCodeByName = (country: string) => {
-  const matchedCountry = countryCodeList.find(
-    (item) => country && item.name.toLowerCase() === country.toLowerCase()
-  );
-  return matchedCountry ? matchedCountry.code : "";
-};
-export default useGetCountryCodeByName;
