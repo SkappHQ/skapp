abd741d994141f8b996cf44666dc9e997e86830c
menura-rootcode <menura.wijesekara@rootcode.io>
2025-10-24 14:16:14 +0530

    chore: update CashIcon height from 14 to 12

--- frontend/src/community/common/assets/Icons/CashIcon.tsx
+++ frontend/src/community/common/assets/Icons/CashIcon.tsx
@@ -6,7 +6,7 @@ const CashIcon = ({ fill = "black" }: IconProps): JSX.Element => {
   return (
     <svg
       width="18"
-      height="14"
+      height="12"
       viewBox="0 0 18 12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
