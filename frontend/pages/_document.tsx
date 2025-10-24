3fde0a4541e0652f3e432248f4c0d6b22db5d584
hasalarootcode <hasala.abhilasha@rootcode.io>
2025-10-24 11:38:01 +0530

    Revert "feat: added meta pixel analytics script"
    This reverts commit c535373416e1c5def0469114d0c3f401bc2ad376.

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -64,23 +64,6 @@ export default function Document() {
             `
           }}
         />
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1805191770407375');
-              fbq('track', 'PageView');
-            `
-          }}
-        />
       </Head>
       <body>
         <noscript>
@@ -90,16 +73,6 @@ export default function Document() {
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
-        </noscript>
-        {/* Meta Pixel noscript fallback */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1805191770407375&ev=PageView&noscript=1"
-            alt=""
-          />
         </noscript>
         <Main />
         <NextScript />
