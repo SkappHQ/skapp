81064d739addd1d9d9ecbfcce59bf30c517295ec
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2025-11-12 13:10:09 +0530

    [Invoice Module] : Expose Internal Endpoint to Post Customer-Project Mapping

---
+++ invoice/controller/v1/InternalProjectController.java
@@ -0,0 +1,33 @@
+package com.skapp.enterprise.invoice.controller.v1;
+
+import com.skapp.community.common.payload.response.ResponseEntityDto;
+import com.skapp.enterprise.invoice.payload.request.InternalProjectCreationRequestDto;
+import com.skapp.enterprise.invoice.service.InternalProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/internal/v1/ep/invoice/project")
+public class InternalProjectController {
+
+	private final InternalProjectService internalProjectService;
+
+	@PostMapping
+	@PreAuthorize("hasAnyRole('ROLE_INTERNAL_API')")
+	public ResponseEntity<ResponseEntityDto> addProjectToCustomer(
+			@RequestBody InternalProjectCreationRequestDto internalProjectCreationRequestDto) {
+
+		ResponseEntityDto response = internalProjectService.createProjectForCustomer(internalProjectCreationRequestDto);
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+}
---
+++ invoice/service/impl/InternalProjectServiceImpl.java
@@ -0,0 +1,77 @@
+package com.skapp.enterprise.invoice.service.impl;
+
+import com.skapp.community.common.exception.EntityNotFoundException;
+import com.skapp.community.common.exception.ModuleException;
+import com.skapp.community.common.payload.response.ResponseEntityDto;
+import com.skapp.enterprise.invoice.constant.InvoiceMessageConstant;
+import com.skapp.enterprise.invoice.mapper.ProjectMapper;
+import com.skapp.enterprise.invoice.model.Customer;
+import com.skapp.enterprise.invoice.model.Project;
+import com.skapp.enterprise.invoice.model.ProjectKey;
+import com.skapp.enterprise.invoice.payload.request.InternalProjectCreationRequestDto;
+import com.skapp.enterprise.invoice.payload.response.project.InternalProjectCreationResponseDto;
+import com.skapp.enterprise.invoice.repository.CustomerDao;
+import com.skapp.enterprise.invoice.repository.ProjectDao;
+import com.skapp.enterprise.invoice.service.InternalProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class InternalProjectServiceImpl implements InternalProjectService {
+
+	private final CustomerDao customerDao;
+
+	private final ProjectDao projectDao;
+
+	private final ProjectMapper projectMapper;
+
+	@Override
+	public ResponseEntityDto createProjectForCustomer(
+			InternalProjectCreationRequestDto internalProjectCreationRequestDto) {
+
+		Optional<Customer> optionalCustomer = customerDao.findById(internalProjectCreationRequestDto.getCustomerId());
+
+		if (optionalCustomer.isEmpty()) {
+			throw new EntityNotFoundException(InvoiceMessageConstant.INVOICE_ERROR_CUSTOMER_NOT_FOUND);
+		}
+
+		Project savedProject = projectDao
+			.save(initializeProjectMapping(optionalCustomer.get(), internalProjectCreationRequestDto.getProjectId()));
+
+		InternalProjectCreationResponseDto responseDto = projectMapper
+			.projectToInternalProjectCreationResponseDto(savedProject);
+
+		return new ResponseEntityDto(false, responseDto);
+
+	}
+
+	private Project initializeProjectMapping(Customer customer, Long projectId) {
+
+		Optional<Project> existingProjects = projectDao.findById_ProjectId(projectId);
+
+		Map<Long, Project> projectMap = existingProjects.stream()
+			.collect(Collectors.toMap(project -> project.getId().getProjectId(), project -> project));
+
+		// Validate if the project is already mapped to any other customer
+		Project existingProject = projectMap.get(projectId);
+		if (existingProject != null && existingProject.getId().getCustomer() != null) {
+			throw new ModuleException(InvoiceMessageConstant.INVOICE_ERROR_VALIDATION_CUSTOMER_PROJECT_MAPPING_INVALID);
+		}
+
+		ProjectKey projectKey = new ProjectKey();
+		projectKey.setProjectId(projectId);
+		projectKey.setCustomer(customer);
+
+		// Create and map the project
+		Project project = new Project();
+		project.setId(projectKey);
+
+		return project;
+	}
+
+}
