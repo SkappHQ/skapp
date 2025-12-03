6ff4612e11996c7aa47d8faddeb53d4d48c16825
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2025-12-03 13:53:17 +0530

    [Invoice Module]: skapp-539- add validations to invoice signed URL

--- common/service/impl/AmazonS3FileValidationServiceImpl.java
+++ common/service/impl/AmazonS3FileValidationServiceImpl.java
@@ -7,7 +7,6 @@ import com.skapp.enterprise.common.service.AmazonS3FileValidationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-
 @Service
 @RequiredArgsConstructor
 public class AmazonS3FileValidationServiceImpl implements AmazonS3FileValidationService {
