6c99cc2c5410b4a378bc049d7d9b9e90165016f3
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-12-19 10:49:18 +0530

    Fixed bugs

--- people/repository/impl/EpEmployeeRepositoryImpl.java
+++ people/repository/impl/EpEmployeeRepositoryImpl.java
@@ -7,6 +7,7 @@ import com.skapp.community.peopleplanner.model.Employee;
 import com.skapp.community.peopleplanner.model.EmployeeRole;
 import com.skapp.community.peopleplanner.model.EmployeeRole_;
 import com.skapp.community.peopleplanner.model.Employee_;
+import com.skapp.community.peopleplanner.repository.impl.EmployeeRepositoryImpl;
 import com.skapp.community.peopleplanner.type.AccountStatus;
 import com.skapp.enterprise.common.model.ModuleConfig;
 import com.skapp.enterprise.people.repository.EpEmployeeRepository;
@@ -17,7 +18,7 @@ import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -25,10 +26,15 @@ import java.util.Arrays;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class EpEmployeeRepositoryImpl implements EpEmployeeRepository {
+@Primary
+public class EpEmployeeRepositoryImpl extends EmployeeRepositoryImpl implements EpEmployeeRepository {
 
 	private final EntityManager entityManager;
+
+	public EpEmployeeRepositoryImpl(EntityManager entityManager, EntityManager entityManager1) {
+		super(entityManager);
+		this.entityManager = entityManager1;
+	}
 
 	@Override
 	public List<Employee> getManagerRoleEmployeesExcludingEmployeeIds(List<Long> employeeIds) {
@@ -115,6 +121,13 @@ public class EpEmployeeRepositoryImpl implements EpEmployeeRepository {
 		query.select(employeeRoot).distinct(true);
 
 		return entityManager.createQuery(query).getResultList();
+	}
+
+	@Override
+	protected void buildEnterprisePredicates(CriteriaBuilder criteriaBuilder, Root<Employee> root,
+			Join<Employee, User> userJoin, List<Predicate> predicates) {
+		Join<Employee, EmployeeRole> employeeRoleJoin = root.join(Employee_.employeeRole);
+		predicates.add(criteriaBuilder.notEqual(employeeRoleJoin.get(EmployeeRole_.PM_ROLE), Role.PM_GUEST_EMPLOYEE));
 	}
 
 }
