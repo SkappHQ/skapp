631cbee2cb263bcf8d9bc379f40496f33df17a61
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-01-02 20:07:34 +0530

    [Esign Module]: skapp-939: Add entity mapping to Document

--- esignature/model/Document.java
+++ esignature/model/Document.java
@@ -1,15 +1,6 @@
 package com.skapp.enterprise.esignature.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -50,5 +41,8 @@ public class Document {
 
 	@Column(name = "num_of_pages")
 	private int numOfPages;
+
+	@OneToOne(mappedBy = "document", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private EsignVerification esignVerification;
 
 }
