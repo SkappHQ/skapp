b8c10ac2fab8b9eb2e3bee6871f5900fa29757c5
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-01-02 20:06:30 +0530

    [Esign Module]: skapp-939: Add entity mapping to recipient

--- esignature/model/Recipient.java
+++ esignature/model/Recipient.java
@@ -6,18 +6,7 @@ import com.skapp.enterprise.esignature.type.EmailStatus;
 import com.skapp.enterprise.esignature.type.InboxStatus;
 import com.skapp.enterprise.esignature.type.MemberRole;
 import com.skapp.enterprise.esignature.type.RecipientStatus;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -95,5 +84,8 @@ public class Recipient {
 	@Enumerated(EnumType.STRING)
 	@Column(name = "mfa_verification_method")
 	private EsignVerificationType mfaVerificationMethod;
+
+	@OneToOne(mappedBy = "recipient", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private EsignVerification esignVerification;
 
 }
