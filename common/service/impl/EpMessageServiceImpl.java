ab2dadafddee5cd09ac780362f5d0300580773f9
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-01-04 18:31:19 +0530

    [Esign Module]: skapp-939: Twilio exception handling.

--- common/service/impl/EpMessageServiceImpl.java
+++ common/service/impl/EpMessageServiceImpl.java
@@ -1,8 +1,12 @@
 package com.skapp.enterprise.common.service.impl;
 
+import com.skapp.community.common.constant.CommonMessageConstant;
 import com.skapp.community.common.exception.ModuleException;
+import com.skapp.community.common.exception.TooManyRequestsException;
 import com.skapp.enterprise.common.constant.EPCommonMessageConstant;
 import com.skapp.enterprise.common.service.EpMessageService;
+import com.twilio.exception.ApiException;
+import com.twilio.exception.AuthenticationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,11 +29,22 @@ public class EpMessageServiceImpl implements EpMessageService {
 			Message message = Message.creator(new PhoneNumber(target), new PhoneNumber(phoneNumber), messageContent)
 				.create();
 
+			log.info("Message sent to: {} successfully.", target);
+
+		}
+		catch (AuthenticationException e) {
+			log.error("Twilio authentication failed: {}", e.getMessage(), e);
+		}
+		catch (ApiException e) {
+			if (e.getStatusCode() == 429) {
+				throw new TooManyRequestsException(CommonMessageConstant.COMMON_ERROR_TOO_MANY_REQUESTS_EXCEPTION);
+			}
+			else {
+				log.error("Error sending message to {}: {}", target, e.getMessage());
+			}
 		}
 		catch (Exception e) {
-			log.error("Unexpected error while sending message : {}", e.getMessage(), e);
-			throw new ModuleException(EPCommonMessageConstant.EP_COMMON_SEND_MESSAGE_ERROR,
-					new String[] { e.getMessage() });
+			log.error("Unexpected error while sending message: {}", e.getMessage(), e);
 		}
 	}
 
