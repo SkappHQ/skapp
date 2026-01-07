ea76efdcfe10352f5966720ae6d9a82bba26926e
Akila Silva <akila.silva@rootcode.io>
2026-01-07 11:25:49 +0530

    fix: validation email issues

--- common/service/ValidationService.java
+++ common/service/ValidationService.java
@@ -1,13 +1,12 @@
 package com.skapp.enterprise.common.service;
 
 import com.skapp.community.common.payload.response.ResponseEntityDto;
-import com.skapp.enterprise.common.payload.response.ValidationResult;
 
 public interface ValidationService {
 
 	ResponseEntityDto validateBusinessEmail(String email);
 
-	ValidationResult validateEmail(String email);
+	void validateEmail(String email);
 
 	void checkBusinessEmailValidity(String email);
 
