4c9cbf3a0364671e57603bc49d38b1f4fb44b2e8
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-01-09 11:09:41 +0530

    [Esign Module]: skapp-939: Null Check Added.

--- esignature/service/impl/EsignMessageServiceImpl.java
+++ esignature/service/impl/EsignMessageServiceImpl.java
@@ -1,5 +1,7 @@
 package com.skapp.enterprise.esignature.service.impl;
 
+import com.skapp.community.common.exception.ModuleException;
+import com.skapp.enterprise.common.constant.EPCommonMessageConstant;
 import com.skapp.enterprise.common.service.EpMessageService;
 import com.skapp.enterprise.esignature.constant.EsignConstants;
 import com.skapp.enterprise.esignature.service.EsignMessageService;
@@ -16,6 +18,16 @@ public class EsignMessageServiceImpl implements EsignMessageService {
 
 	@Override
 	public void sendOtpMessage(String target, String otp) {
+
+		if (target == null) {
+			throw new ModuleException(EPCommonMessageConstant.EP_COMMON_SEND_MESSAGE_ERROR_TARGET_NOT_AVAILABLE);
+		}
+
+		if (otp == null) {
+			throw new ModuleException(EPCommonMessageConstant.EP_COMMON_SEND_MESSAGE_ERROR_CONTENT_NOT_AVAILABLE);
+
+		}
+
 		epMessageService.sendMessage(target, otp);
 	}
 
