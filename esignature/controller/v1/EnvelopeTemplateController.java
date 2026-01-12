445527d47b8a520b5c5448caf071fafb6bbc5e0a
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-01-12 14:17:52 +0530

    [Esign Module]: sign-16: Optimize imports

--- esignature/controller/v1/EnvelopeTemplateController.java
+++ esignature/controller/v1/EnvelopeTemplateController.java
@@ -4,14 +4,18 @@ import com.skapp.community.common.payload.response.ResponseEntityDto;
 import com.skapp.enterprise.esignature.payload.request.template.EnvelopeTemplateDto;
 import com.skapp.enterprise.esignature.service.EnvelopeTemplateService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
