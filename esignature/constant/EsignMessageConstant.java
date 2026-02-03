22639b810dfcae2a1ef46fbe23601a940e752142
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-02-03 16:19:06 +0530

    [Esign Module]: sign-51: update messages

--- esignature/constant/EsignMessageConstant.java
+++ esignature/constant/EsignMessageConstant.java
@@ -220,10 +220,12 @@ public enum EsignMessageConstant implements MessageConstant {
 	ESIGN_ERROR_ENVELOPE_TEMPLATE_LEAST_ONE_RECIPIENTS_ROLE_REQUIRED(
 			"ep.esign.error.envelope-template.least.one-recipient.role.required"),
 
-	ESIGN_ERROR_DROPDOWN_FIELD_MUST_HAVE_AT_LEAST_1_OPTION("ep.esign.error.dropdown-field.must.have.at.least.1.option"),
+	ESIGN_ERROR_DROPDOWN_FIELD_MUST_HAVE_AT_LEAST_1_OPTION(
+			"ep.esign.error.dropdown-field.must.have.at.least.one.option"),
 	ESIGN_ERROR_RADIO_BUTTON_FIELD_MUST_HAVE_AT_LEAST_2_OPTIONS(
-			"ep.esign.error.radio-button-field.must.have.at.least.2.options"),
-	ESIGN_ERROR_CHECKBOX_FIELD_MUST_HAVE_AT_LEAST_1_OPTION("ep.esign.error.checkbox-field.must.have.at.least.1.option"),
+			"ep.esign.error.radio-button-field.must.have.at.least.two.options"),
+	ESIGN_ERROR_CHECKBOX_FIELD_MUST_HAVE_AT_LEAST_1_OPTION(
+			"ep.esign.error.checkbox-field.must.have.at.least.one.option"),
 	ESIGN_ERROR_FIELD_OPTION_VALUE_EXCEEDS_MAX_LENGTH("ep.esign.error.field.option.value.exceeds.max.length"),
 	ESIGN_ERROR_FIELD_OPTION_VALUE_MUST_BE_UNIQUE("ep.esign.error.field.option.value.must.be.unique"),
 	ESIGN_ERROR_FIELD_OPTION_DISPLAY_ORDER_MUST_BE_UNIQUE("ep.esign.error.field.option.display.order.must.be.unique"),
