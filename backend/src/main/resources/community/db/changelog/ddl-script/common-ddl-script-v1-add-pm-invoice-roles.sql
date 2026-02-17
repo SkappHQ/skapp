6699fa32193aeb15df9e09de2afe74d8d7be7a7f
udulaThathsaridu <udula.senevirathna@rootcode.io>
2026-02-17 15:57:37 +0530

    fix/community-be-fixes

---
+++ backend/src/main/resources/community/db/changelog/ddl-script/common-ddl-script-v1-add-pm-invoice-roles.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset system:common-ddl-script-v1-add-pm-invoice-roles
+ALTER TABLE `employee_role`
+    ADD COLUMN `pm_role` varchar(255) DEFAULT NULL;
+    ADD COLUMN `invoice_role` varchar(255) DEFAULT NULL;
+;
+
+-- rollback ALTER TABLE `employee_role` DROP COLUMN `pm_role`, DROP COLUMN `invoice_role`;
