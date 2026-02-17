6e7098c9bd7dc68c351722649e5e8e743fd1c2e2
udulaThathsaridu <udula.senevirathna@rootcode.io>
2026-02-17 16:18:34 +0530

    fix/correct-syntax

--- backend/src/main/resources/community/db/changelog/ddl-script/common-ddl-script-v1-add-pm-invoice-roles.sql
+++ backend/src/main/resources/community/db/changelog/ddl-script/common-ddl-script-v1-add-pm-invoice-roles.sql
@@ -2,8 +2,8 @@
 
 -- changeset system:common-ddl-script-v1-add-pm-invoice-roles
 ALTER TABLE `employee_role`
-    ADD COLUMN `pm_role` varchar(255) DEFAULT NULL;
-    ADD COLUMN `invoice_role` varchar(255) DEFAULT NULL;
+    ADD COLUMN `pm_role` varchar(255) DEFAULT NULL,
+    ADD COLUMN `invoice_role` varchar(255) DEFAULT NULL
 ;
 
 -- rollback ALTER TABLE `employee_role` DROP COLUMN `pm_role`, DROP COLUMN `invoice_role`;
