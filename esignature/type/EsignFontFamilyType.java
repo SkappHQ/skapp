6cb7db21ece77ad409d49400b1a6452e08f43a33
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-03-27 15:24:17 +0530

    [Esign Module]: address enum/constant comments.

--- esignature/type/EsignFontFamilyType.java
+++ esignature/type/EsignFontFamilyType.java
@@ -1,13 +1,5 @@
 package com.skapp.enterprise.esignature.type;
 
-import static com.skapp.enterprise.esignature.constant.EsignConstants.FONT_FOLDER_DEJAVU_SANS;
-import static com.skapp.enterprise.esignature.constant.EsignConstants.FONT_SUFFIX_BOLD;
-import static com.skapp.enterprise.esignature.constant.EsignConstants.FONT_SUFFIX_BOLD_ITALIC;
-import static com.skapp.enterprise.esignature.constant.EsignConstants.FONT_SUFFIX_BOLD_OBLIQUE;
-import static com.skapp.enterprise.esignature.constant.EsignConstants.FONT_SUFFIX_ITALIC;
-import static com.skapp.enterprise.esignature.constant.EsignConstants.FONT_SUFFIX_OBLIQUE;
-import static com.skapp.enterprise.esignature.constant.EsignConstants.FONT_SUFFIX_REGULAR;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -15,12 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum EsignFontFamilyType {
 
-	ARIMO("Arimo", FONT_SUFFIX_REGULAR, FONT_SUFFIX_ITALIC, FONT_SUFFIX_BOLD, FONT_SUFFIX_BOLD_ITALIC),
-	CARLITO("Carlito", FONT_SUFFIX_REGULAR, FONT_SUFFIX_ITALIC, FONT_SUFFIX_BOLD, FONT_SUFFIX_BOLD_ITALIC),
-	TINOS("Tinos", FONT_SUFFIX_REGULAR, FONT_SUFFIX_ITALIC, FONT_SUFFIX_BOLD, FONT_SUFFIX_BOLD_ITALIC),
-	DEJAVU_SANS(FONT_FOLDER_DEJAVU_SANS, "", FONT_SUFFIX_OBLIQUE, FONT_SUFFIX_BOLD, FONT_SUFFIX_BOLD_OBLIQUE),
-	INTER("Inter", FONT_SUFFIX_REGULAR, FONT_SUFFIX_ITALIC, FONT_SUFFIX_BOLD, FONT_SUFFIX_BOLD_ITALIC),
-	POPPINS("Poppins", FONT_SUFFIX_REGULAR, FONT_SUFFIX_ITALIC, FONT_SUFFIX_BOLD, FONT_SUFFIX_BOLD_ITALIC);
+	ARIMO("Arimo", "-Regular", "-Italic", "-Bold", "-BoldItalic"),
+	CARLITO("Carlito", "-Regular", "-Italic", "-Bold", "-BoldItalic"),
+	TINOS("Tinos", "-Regular", "-Italic", "-Bold", "-BoldItalic"),
+	DEJAVU_SANS("DejaVuSans", "", "-Oblique", "-Bold", "-BoldOblique"),
+	INTER("Inter", "-Regular", "-Italic", "-Bold", "-BoldItalic"),
+	POPPINS("Poppins", "-Regular", "-Italic", "-Bold", "-BoldItalic");
 
 	private final String folderName;
 
