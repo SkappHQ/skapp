8a0602abcec201e82cccc0ebd4a9bdcfa5415676
Thusala Piyarisi <thusala.piyarisi@rootcode.io>
2026-04-22 10:44:39 +0530

    refactor: remove unused annotations from NotificationTypeCountResponseDto

--- backend/src/main/java/com/skapp/community/common/payload/response/NotificationTypeCountResponseDto.java
+++ backend/src/main/java/com/skapp/community/common/payload/response/NotificationTypeCountResponseDto.java
@@ -3,12 +3,8 @@ package com.skapp.community.common.payload.response;
 import com.skapp.community.common.type.NotificationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class NotificationTypeCountResponseDto {
 
