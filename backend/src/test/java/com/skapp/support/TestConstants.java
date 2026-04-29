84d035b79e201cec3831d3d31a6a972325bcb110
Akila Silva <akila.silva@rootcode.io>
2026-04-29 12:03:51 +0530

    refactor: add API key constants to TestConstants

--- backend/src/test/java/com/skapp/support/TestConstants.java
+++ backend/src/test/java/com/skapp/support/TestConstants.java
@@ -5,6 +5,10 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class TestConstants {
 
+	public static final String API_KEY_HEADER = "x-api-key";
+
+	public static final String VALID_API_KEY = "test-api-key-12345";
+
 	public static final String STATUS_PATH = "['status']";
 
 	public static final String RESULTS_PATH = "['results']";
