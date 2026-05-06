d061395fa77c202831fba4dc4d78290cce0ba768
Shakila Kamalasena <shakila.kamalasena@rootcode.io>
2026-05-06 18:55:29 +0530

    feat: add WorkLocationDao to EpPeopleServiceImpl for work location management

--- people/service/impl/EpPeopleServiceImpl.java
+++ people/service/impl/EpPeopleServiceImpl.java
@@ -40,6 +40,7 @@ import com.skapp.community.peopleplanner.repository.EmployeeTeamDao;
 import com.skapp.community.peopleplanner.repository.JobFamilyDao;
 import com.skapp.community.peopleplanner.repository.JobTitleDao;
 import com.skapp.community.peopleplanner.repository.TeamDao;
+import com.skapp.community.common.repository.WorkLocationDao;
 import com.skapp.community.peopleplanner.service.EmployeeValidationService;
 import com.skapp.community.peopleplanner.service.PeopleEmailService;
 import com.skapp.community.peopleplanner.service.RolesService;
@@ -155,9 +156,9 @@ public class EpPeopleServiceImpl extends PeopleServiceImpl implements EpPeopleSe
 
 	public EpPeopleServiceImpl(UserService userService, MessageUtil messageUtil, PeopleMapper peopleMapper,
 			UserDao userDao, TeamDao teamDao, EmployeeDao employeeDao, JobFamilyDao jobFamilyDao,
-			JobTitleDao jobTitleDao, EmployeePeriodDao employeePeriodDao, EmployeeTeamDao employeeTeamDao,
-			EmployeeManagerDao employeeManagerDao, PasswordEncoder passwordEncoder, RolesService rolesService,
-			PageTransformer pageTransformer, PlatformTransactionManager transactionManager,
+			JobTitleDao jobTitleDao, WorkLocationDao workLocationDao, EmployeePeriodDao employeePeriodDao,
+			EmployeeTeamDao employeeTeamDao, EmployeeManagerDao employeeManagerDao, PasswordEncoder passwordEncoder,
+			RolesService rolesService, PageTransformer pageTransformer, PlatformTransactionManager transactionManager,
 			PeopleEmailService peopleEmailService, JsonMapper mapper,
 			EncryptionDecryptionService encryptionDecryptionService, BulkContextService bulkContextService,
 			AsyncEmailServiceImpl asyncEmailServiceImpl, ApplicationEventPublisher applicationEventPublisher,
@@ -173,10 +174,10 @@ public class EpPeopleServiceImpl extends PeopleServiceImpl implements EpPeopleSe
 			EnvelopeService envelopeService, CacheService cacheService, EpOrganizationDao epOrganizationDao,
 			EmployeeExportMapperService employeeExportMapperService) {
 		super(userService, messageUtil, peopleMapper, userDao, teamDao, employeeDao, jobFamilyDao, jobTitleDao,
-				employeePeriodDao, employeeTeamDao, employeeManagerDao, passwordEncoder, rolesService, pageTransformer,
-				transactionManager, peopleEmailService, mapper, encryptionDecryptionService, bulkContextService,
-				asyncEmailServiceImpl, applicationEventPublisher, userVersionService, employeeValidationService,
-				employeeExportMapperService);
+				workLocationDao, employeePeriodDao, employeeTeamDao, employeeManagerDao, passwordEncoder, rolesService,
+				pageTransformer, transactionManager, peopleEmailService, mapper, encryptionDecryptionService,
+				bulkContextService, asyncEmailServiceImpl, applicationEventPublisher, userVersionService,
+				employeeValidationService, employeeExportMapperService);
 		this.employeeDao = employeeDao;
 		this.epEmployeeDao = epEmployeeDao;
 		this.employeeRoleDao = employeeRoleDao;
