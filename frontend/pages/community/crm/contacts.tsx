627d25f04e1ebc10275ff9062afa09b792c3de14
sanjula-rtc <sanjula.lakpahana@rootcode.io>
2026-05-07 12:47:05 +0530

    feat: implement inital contact modal functionality and update CRM store

--- frontend/pages/community/crm/contacts.tsx
+++ frontend/pages/community/crm/contacts.tsx
@@ -3,9 +3,20 @@ import { NextPage } from "next";
 import ContentLayout from "~community/common/components/templates/ContentLayout/ContentLayout";
 import { useTranslator } from "~community/common/hooks/useTranslator";
 import { IconName } from "~community/common/types/IconTypes";
+import CrmContactsModalController from "~community/crm/components/organisms/CrmContactsModalController/CrmContactsModalController";
+import { useCrmStore } from "~community/crm/store/crmStore";
+import { CrmModalTypes } from "~community/crm/types/ModalTypes";
 
 const CrmContacts: NextPage = () => {
   const translateText = useTranslator("crmModule", "contacts");
+  const { setIsAddContactModalOpen, setCrmModalType } = useCrmStore(
+    (state) => state
+  );
+
+  const handleAddContact = () => {
+    setIsAddContactModalOpen(true);
+    setCrmModalType(CrmModalTypes.ADD_CONTACT_MODAL);
+  };
 
   return (
     <ContentLayout
@@ -13,8 +24,12 @@ const CrmContacts: NextPage = () => {
       title={translateText(["title"])}
       primaryButtonText={translateText(["addContactBtn"])}
       primaryBtnIconName={IconName.ADD_ICON}
+      onPrimaryButtonClick={handleAddContact}
     >
-      {/* CRM Contacts content */}
+      <>
+        {/* CRM Contacts content */}
+        <CrmContactsModalController />
+      </>
     </ContentLayout>
   );
 };
--- frontend/src/community/crm/types/CrmStoreTypes.ts
+++ frontend/src/community/crm/types/CrmStoreTypes.ts
@@ -3,9 +3,13 @@ import { CrmModalTypes } from "./ModalTypes";
 interface actionsTypes {
     setIsAddCompanyModalOpen: (isAddCompanyModalOpen: boolean) => void;
     setCompanyModalType: (companyModalType: CrmModalTypes) => void;
+    setCrmModalType: (crmModalType: CrmModalTypes) => void;
+    setIsAddContactModalOpen: (isAddContactModalOpen: boolean) => void;
 }
 
 export interface CrmStore extends actionsTypes {
     isAddCompanyModalOpen: boolean;
     companyModalType: CrmModalTypes;
+    crmModalType: CrmModalTypes;
+    isAddContactModalOpen: boolean;
 }
