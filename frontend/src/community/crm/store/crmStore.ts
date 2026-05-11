3808f963cdf87b42a0188cbd67f32ab17880d6b4
Charith Kularatne <charith.kularatne@rootcode.io>
2026-05-11 18:02:14 +0530

    feat: addressed comments

--- frontend/src/community/crm/store/crmStore.ts
+++ frontend/src/community/crm/store/crmStore.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
 import { CrmStore } from "../types/CrmStoreTypes";
-import CrmCompanyModalSlice from "./slices/CrmCompanyModalSlice";
+import CrmCompanyModalSlice from "./slices/crmCompanyModalSlice";
 
 export const useCrmStore = create<
   CrmStore,
