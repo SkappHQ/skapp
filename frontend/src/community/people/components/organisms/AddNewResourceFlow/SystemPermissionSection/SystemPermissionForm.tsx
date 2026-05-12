ff2e2f011f2b3414463ea1ddd73cd59e4b26c61b
anusham <anusha.mahindarathne@rootcode.io>
2026-05-12 19:48:25 +0530

    feat: add CRM role support to system permission forms and handlers

--- frontend/src/community/people/components/organisms/AddNewResourceFlow/SystemPermissionSection/SystemPermissionForm.tsx
+++ frontend/src/community/people/components/organisms/AddNewResourceFlow/SystemPermissionSection/SystemPermissionForm.tsx
@@ -378,7 +378,8 @@ const SystemPermissionForm = ({
               inputName={"crmRole"}
               label={systemPermissionsText(["crm"])}
               itemList={grantablePermission?.crm || []}
-              value={values.crmRole}
+              placeholder={systemPermissionsText(["selectRole"])}
+              value={values.crmRole === Role.CRM_NONE ? "" : values.crmRole}
               componentStyle={classes.dropdownListComponentStyles}
               checkSelected
               onChange={(event) =>
