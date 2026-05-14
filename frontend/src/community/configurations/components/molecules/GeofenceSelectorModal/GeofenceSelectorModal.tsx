8e13333e5b80fa297f365f4dbd4ae5c7394d328e
Thusala Piyarisi <thusala.piyarisi@rootcode.io>
2026-05-14 09:51:13 +0530

    fix: height issue

--- frontend/src/community/configurations/components/molecules/GeofenceSelectorModal/GeofenceSelectorModal.tsx
+++ frontend/src/community/configurations/components/molecules/GeofenceSelectorModal/GeofenceSelectorModal.tsx
@@ -160,7 +160,7 @@ const GeofenceSelectorModal = ({ formik }: Props) => {
       <GeofenceMapView
         center={tempMarkerPosition}
         radius={tempRadius}
-        height="22rem"
+        height="30rem"
         mapId="geofence-map"
         onClick={handleMapClick}
       >
