f8b05e489eb605f088b04e85e9257bab1afaf98e
Thusala Piyarisi <thusala.piyarisi@rootcode.io>
2026-05-14 10:20:18 +0530

    feat: add geofencing settings to attendance configuration

--- frontend/src/fallback/configurations/components/organisms/GeoFencingSettings/GeoFencingSettings.tsx
+++ frontend/src/fallback/configurations/components/organisms/GeoFencingSettings/GeoFencingSettings.tsx
@@ -1,5 +1,20 @@
-const GeoFencingSettings = () => {
-  return <div></div>;
+import { AttendanceConfigurationType } from "~community/attendance/types/attendanceTypes";
+
+interface Props {
+  config: AttendanceConfigurationType | null;
+  initialConfig: AttendanceConfigurationType | null;
+  onSwitchChange: (
+    key: keyof AttendanceConfigurationType,
+    checked: boolean
+  ) => void;
+}
+
+const GeoFencingSettings = ({
+  config,
+  initialConfig,
+  onSwitchChange
+}: Props) => {
+  return <></>;
 };
 
 export default GeoFencingSettings;
