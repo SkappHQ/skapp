7eb5cef689e8b13340f8bd5b1de96d8756493210
Sanjula <sanjula.lakpahana@rootcode.io>
2026-05-15 11:36:26 +0000

    chore: revert pages

--- frontend/pages/community/crm/contacts.tsx
+++ frontend/pages/community/crm/contacts.tsx
@@ -1,22 +1,22 @@
-// import { NextPage } from "next";
+import { NextPage } from "next";
 
-// import ContentLayout from "~community/common/components/templates/ContentLayout/ContentLayout";
-// import { useTranslator } from "~community/common/hooks/useTranslator";
-// import { IconName } from "~community/common/types/IconTypes";
+import ContentLayout from "~community/common/components/templates/ContentLayout/ContentLayout";
+import { useTranslator } from "~community/common/hooks/useTranslator";
+import { IconName } from "~community/common/types/IconTypes";
 
-// const Contacts: NextPage = () => {
-//   const translateText = useTranslator("crmModule", "contacts");
+const Contacts: NextPage = () => {
+  const translateText = useTranslator("crmModule", "contacts");
 
-//   return (
-//     <ContentLayout
-//       pageHead={translateText(["pageHead"])}
-//       title={translateText(["title"])}
-//       primaryButtonText={translateText(["addContactBtn"])}
-//       primaryBtnIconName={IconName.ADD_ICON}
-//     >
-//       <></>
-//     </ContentLayout>
-//   );
-// };
+  return (
+    <ContentLayout
+      pageHead={translateText(["pageHead"])}
+      title={translateText(["title"])}
+      primaryButtonText={translateText(["addContactBtn"])}
+      primaryBtnIconName={IconName.ADD_ICON}
+    >
+      <></>
+    </ContentLayout>
+  );
+};
 
-// export default Contacts;
+export default Contacts;
