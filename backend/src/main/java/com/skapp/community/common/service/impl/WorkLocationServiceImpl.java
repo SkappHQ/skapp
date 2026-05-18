3317fc6eb170e104f03d412f4f4c4bba2b1f0515
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-05-18 10:45:22 +0530

    [People Module]: Make Holiday Add Email send to Async/Sort worklocations by name

--- backend/src/main/java/com/skapp/community/common/service/impl/WorkLocationServiceImpl.java
+++ backend/src/main/java/com/skapp/community/common/service/impl/WorkLocationServiceImpl.java
@@ -28,7 +28,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
 
 @Service
 @Slf4j
