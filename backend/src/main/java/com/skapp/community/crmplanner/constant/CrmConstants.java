226bd35a4c77220feb3b91e158fadca426a0c648
Charith Kularatne <charith.kularatne@rootcode.io>
2026-05-21 13:36:18 +0530

    fix: removed unused constants

--- backend/src/main/java/com/skapp/community/crmplanner/constant/CrmConstants.java
+++ backend/src/main/java/com/skapp/community/crmplanner/constant/CrmConstants.java
@@ -13,40 +13,8 @@ public class CrmConstants {
 
 	public static final int PHONE_MAX_LENGTH = 15;
 
-	public static final String ID = "id";
-
-	public static final String NAME = "name";
-
-	public static final String INDUSTRY = "industry";
-
-	public static final String WEBSITE = "website";
-
-	public static final String ADDRESS = "address";
-
-	public static final String CONTACT_NUMBER = "contactNumber";
-
-	public static final String IS_DELETED = "isDeleted";
-
-	public static final String TASKS = "tasks";
-
-	public static final String DEALS = "deals";
-
-	public static final String IS_COMPLETED = "isCompleted";
-
-	public static final String DUE_AT = "dueAt";
-
-	public static final String STAGE = "stage";
-
-	public static final String STAGE_TYPE = "stageType";
-
-	public static final String STAGE_ID = "id";
-
 	public static final String WON_STAGE_TYPE = "WON";
 
 	public static final Long DEFAULT_WON_STAGE_ID = 5L;
-
-	public static final String AMOUNT = "amountNumeric";
-
-	public static final String COMPANY = "company";
 
 }
