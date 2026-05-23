7aa9dd2b781a52ffa50f12ffa261513583cd4a32
dulsara-manakal <dulsara.manakal@rootcode.io>
2026-05-23 22:10:31 +0530

    feat: add SidePanelContactHeader and ContactInfoItem components to CRM module

--- frontend/src/community/crm/components/molecules/SidePanelContactHeader/SidePanelContactHeader.tsx
+++ frontend/src/community/crm/components/molecules/SidePanelContactHeader/SidePanelContactHeader.tsx
@@ -3,70 +3,71 @@ import { FC } from "react";
 import { IconName } from "~community/common/types/IconTypes";
 import ContactInfoItem from "~community/crm/components/atoms/SidePanelContactInfoItem/SidePanelContactInfoItem";
 import {
-  ContactHeaderSkeleton
+    ContactHeaderSkeleton
 } from "~community/crm/components/atoms/SidePanelContactInfoItem/ContactInfoItemSkeleton";
 import { ContactDetail } from "~community/crm/types/CommonTypes";
 
 import styles from "./styles";
 import { useTranslator } from "~community/common/hooks/useTranslator";
 import { formatISODateWithSuffixLocal } from "~community/common/utils/dateTimeUtils";
+import { BuildingIcon } from "@rootcodelabs/skapp-ui";
 
 interface Props {
-  contact?: ContactDetail;
-  isLoading?: boolean;
-  onCompanyClick?: (companyId: number) => void;
+    contact?: ContactDetail;
+    isLoading?: boolean;
+    onCompanyClick?: (companyId: number) => void;
 }
 
 const ContactHeader: FC<Props> = ({ contact, isLoading, onCompanyClick }) => {
-  const cls = styles;
-  const translateText = useTranslator(
-    "crmModule",
-    "contacts",
-    "contactDetailsPanel"
-  );
+    const cls = styles;
+    const translateText = useTranslator(
+        "crmModule",
+        "contacts",
+        "contactDetailsPanel"
+    );
 
-  return (
-    <div id="contact-panel-title" className={cls.wrapper}>
-      {isLoading ? (
-        <ContactHeaderSkeleton />
-      ) : contact ? (
-        <>
-          <div className={cls.contactHeader}>
-            <p className={cls.contactName}>
-              {contact.name}
-            </p>
-            <p className={cls.lastUpdated}>
-              {`${translateText(["lastUpdated"])} : ${formatISODateWithSuffixLocal(contact.lastModifiedDate)}`}
-            </p>
-          </div>
-          <div className={cls.infoRow}>
-            <ContactInfoItem icon={IconName.EMAIL_ICON} value={contact.email} />
+    return (
+        <div id="contact-panel-title" className={cls.wrapper}>
+            {isLoading ? (
+                <ContactHeaderSkeleton />
+            ) : contact ? (
+                <>
+                    <div className={cls.contactHeader}>
+                        <p className={cls.contactName}>
+                            {contact.name}
+                        </p>
+                        <p className={cls.lastUpdated}>
+                            {`${translateText(["lastUpdated"])} : ${formatISODateWithSuffixLocal(contact.lastModifiedDate)}`}
+                        </p>
+                    </div>
+                    <div className={cls.infoRow}>
+                        <ContactInfoItem icon={IconName.EMAIL_ICON} value={contact.email} />
 
-            <ContactInfoItem
-              icon={IconName.LOCAL_PHONE_ICON}
-              value={contact.contactNumber}
-            />
+                        <ContactInfoItem
+                            icon={IconName.LOCAL_PHONE_ICON}
+                            value={contact.contactNumber}
+                        />
 
-            {contact.company && (
-              <ContactInfoItem
-                icon={IconName.BUILDING_ICON}
-                value={contact.company.name}
-                // TODO: Open company side panel
-                onClick={
-                  onCompanyClick
-                    ? () => {
-                      if (contact.company) onCompanyClick(contact.company.id);
-                    }
-                    : undefined
-                }
-                endIcon={IconName.POP_OUT_ICON}
-              />
-            )}
-          </div>
-        </>
-      ) : null}
-    </div>
-  );
+                        {contact.company && (
+                            <ContactInfoItem
+                                icon={<BuildingIcon color={cls.iconFill} />}
+                                value={contact.company.name}
+                                // TODO: Open company side panel
+                                onClick={
+                                    onCompanyClick
+                                        ? () => {
+                                            if (contact.company) onCompanyClick(contact.company.id);
+                                        }
+                                        : undefined
+                                }
+                                endIcon={IconName.POP_OUT_ICON}
+                            />
+                        )}
+                    </div>
+                </>
+            ) : null}
+        </div>
+    );
 };
 
 export default ContactHeader;
--- frontend/src/community/crm/components/molecules/SidePanelContactHeader/styles.ts
+++ frontend/src/community/crm/components/molecules/SidePanelContactHeader/styles.ts
@@ -1,9 +1,10 @@
 const styles = {
-  wrapper: "w-full flex flex-col gap-[8px] items-start",
-  infoRow: "flex items-center justify-between max-w-[629px] w-full",
-  contactName: "h1 leading-[24px] tracking-[0.07px] text-black",
-  lastUpdated: "body2 leading-[24px] text-secondary-text",
-  contactHeader: "flex flex-col gap-[8px]"
+    wrapper: "w-full flex flex-col gap-[8px] items-start",
+    infoRow: "flex items-center justify-between max-w-[629px] w-full",
+    contactName: "h1 leading-[24px] tracking-[0.07px] text-black",
+    lastUpdated: "body2 leading-[24px] text-secondary-text",
+    contactHeader: "flex flex-col gap-[8px]",
+    iconFill: "var(--color-secondary-icon)"
 };
 
 export default styles;
