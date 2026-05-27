92d28954d08fe431bac86a80dc612cfe5b5a3b2f
Yuhanga Induwara <yuhanga.induwara@rootcode.io>
2026-05-27 12:33:38 +0530

    Add createCustomer endpoint to InternalCustomerController

--- invoice/controller/v1/InternalCustomerController.java
+++ invoice/controller/v1/InternalCustomerController.java
@@ -1,12 +1,18 @@
 package com.skapp.enterprise.invoice.controller.v1;
 
+import com.skapp.community.common.payload.response.ResponseEntityDto;
+import com.skapp.enterprise.invoice.payload.request.CustomerCreateRequestDto;
 import com.skapp.enterprise.invoice.payload.response.InternalCustomerResponseDto;
+import com.skapp.enterprise.invoice.service.CustomerService;
 import com.skapp.enterprise.invoice.service.InternalCustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +25,23 @@ public class InternalCustomerController {
 
 	private final InternalCustomerService internalCustomerService;
 
+	private final CustomerService customerService;
+
 	@GetMapping
 	@PreAuthorize("hasAnyRole('ROLE_INTERNAL_API')")
 	public ResponseEntity<List<InternalCustomerResponseDto>> getAllCustomers() {
 
 		List<InternalCustomerResponseDto> response = internalCustomerService.findAllCustomers();
 		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAnyRole('ROLE_INTERNAL_API')")
+	public ResponseEntity<ResponseEntityDto> createCustomer(
+			@RequestBody CustomerCreateRequestDto customerCreateRequestDto) {
+
+		ResponseEntityDto response = customerService.createCustomer(customerCreateRequestDto);
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
 	}
 
 }
