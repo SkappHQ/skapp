e2a2134d25917f776663d5eb6607da8e6c11f657
sanjula-rtc <sanjula.lakpahana@rootcode.io>
2026-05-28 16:22:19 +0530

    revert helpers

--- frontend/src/community/crm/utils/tableHelpers.ts
+++ frontend/src/community/crm/utils/tableHelpers.ts
@@ -2,7 +2,7 @@ type NumericValue = string | number | null;
 
 export const formatValue = (value: NumericValue) => {
   if (value == null || value == 0) return "-";
-  return `$${value.toString().split(".")[0]}`;
+  return  `$${value.toString().split('.')[0]}`
 };
 
 export const formatPhoneNumber = (value: NumericValue) => {
@@ -10,7 +10,11 @@ export const formatPhoneNumber = (value: NumericValue) => {
   return `+${value}`;
 };
 
-export const formatTasks = (value: NumericValue) => {
+export const formatTasks = (
+  value: NumericValue
+) => {
   if (value == null || value === 0) return "-";
-  return `${value}`;
+  return (
+    `${value}`
+  );
 };
