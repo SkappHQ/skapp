3c64f38242bcb3c8e9028de6ee37ce8217161aa3
Charith Kularatne <charith.kularatne@rootcode.io>
2026-06-01 09:31:45 +0530

    fix: removed deserializer

--- backend/src/main/java/com/skapp/community/crmplanner/payload/request/CrmCompanyCreateDto.java
+++ backend/src/main/java/com/skapp/community/crmplanner/payload/request/CrmCompanyCreateDto.java
@@ -2,7 +2,6 @@ package com.skapp.community.crmplanner.payload.request;
 
 import com.skapp.enterprise.common.config.TrimmingStringDeserializer;
 import com.skapp.community.crmplanner.type.CrmIndustry;
-import com.skapp.community.crmplanner.util.deserializer.CrmIndustryDeserializer;
 import lombok.Getter;
 import lombok.Setter;
 import tools.jackson.databind.annotation.JsonDeserialize;
@@ -14,7 +13,6 @@ public class CrmCompanyCreateDto {
 	@JsonDeserialize(using = TrimmingStringDeserializer.class)
 	private String name;
 
-	@JsonDeserialize(using = CrmIndustryDeserializer.class)
 	private CrmIndustry industry;
 
 	@JsonDeserialize(using = TrimmingStringDeserializer.class)
