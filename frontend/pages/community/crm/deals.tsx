42c60760e333fd2bf574bf4e0143c41b1d84d7b4
anusham <anusha.mahindarathne@rootcode.io>
2026-06-06 23:41:44 +0530

    feat(crm): integrate add deal side panel into deals page

--- frontend/pages/community/crm/deals.tsx
+++ frontend/pages/community/crm/deals.tsx
@@ -3,10 +3,16 @@ import { NextPage } from "next";
 import ContentLayout from "~community/common/components/templates/ContentLayout/ContentLayout";
 import { useTranslator } from "~community/common/hooks/useTranslator";
 import { IconName } from "~community/common/types/IconTypes";
+import AddDealSidePanel from "~community/crm/components/organisms/AddDealSidePanel/AddDealSidePanel";
 import DealsSection from "~community/crm/components/organisms/DealsSection/DealsSection";
+import { useCrmStore } from "~community/crm/store/store";
 
 const Deals: NextPage = () => {
   const translateText = useTranslator("crmModule", "deals");
+
+  const { openSidePanel } = useCrmStore((store) => ({
+    openSidePanel: store.openSidePanel
+  }));
 
   return (
     <ContentLayout
@@ -14,8 +20,12 @@ const Deals: NextPage = () => {
       title={translateText(["title"])}
       primaryButtonText={translateText(["addDealBtn"])}
       primaryBtnIconName={IconName.ADD_ICON}
+      onPrimaryButtonClick={openSidePanel}
     >
-      <DealsSection />
+      <>
+        <AddDealSidePanel />
+        <DealsSection />
+      </>
     </ContentLayout>
   );
 };
