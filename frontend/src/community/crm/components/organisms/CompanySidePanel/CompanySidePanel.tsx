d54e3413240f0bd3df8b0c04bce93cb90f04ab21
Charith Kularatne <charith.kularatne@rootcode.io>
2026-06-08 17:13:11 +0530

    feat: added kebab menu actions

--- frontend/src/community/crm/components/organisms/CompanySidePanel/CompanySidePanel.tsx
+++ frontend/src/community/crm/components/organisms/CompanySidePanel/CompanySidePanel.tsx
@@ -1,13 +1,78 @@
-import { SidePanel } from "@rootcodelabs/skapp-ui";
+import {
+  DeleteButtonIcon,
+  EditIcon,
+  KebabMenu,
+  SidePanel
+} from "@rootcodelabs/skapp-ui";
 
+import { useTranslator } from "~community/common/hooks/useTranslator";
+import { useCrmStore } from "~community/crm/store/store";
 import { CrmCompanyMetricsType } from "~community/crm/types/CommonTypes";
+import { CrmModalTypes } from "~community/crm/types/ModalTypes";
 
 const CompanySidePanel: React.FC<{
   company: CrmCompanyMetricsType | null;
   isOpen: boolean;
   onClose: () => void;
 }> = ({ isOpen, onClose }) => {
-  return <SidePanel isOpen={isOpen} onClose={onClose} />;
+  const translateText = useTranslator("crmModule", "companies", "sidePanel");
+
+  const { setIsCompanyModalOpen, setCompanyModalType } = useCrmStore(
+    (store) => ({
+      setIsCompanyModalOpen: store.setIsCompanyModalOpen,
+      setCompanyModalType: store.setCompanyModalType
+    })
+  );
+
+  const handleOpenDeleteCompanyModal = () => {
+    setIsCompanyModalOpen(true);
+    setCompanyModalType(CrmModalTypes.DELETE_COMPANY_MODAL);
+  };
+
+  const handleOpenEditCompanyModal = () => {
+    setIsCompanyModalOpen(true);
+    setCompanyModalType(CrmModalTypes.EDIT_COMPANY_MODAL);
+  };
+
+  return (
+    <SidePanel
+      isOpen={isOpen}
+      onClose={onClose}
+      headerActions={
+        <KebabMenu
+          id={"company-actions"}
+          menuItems={[
+            {
+              id: "edit",
+              label: translateText(["editCompany"]),
+              icon: { start: <EditIcon width="16px" height="16px" /> },
+              onClick: handleOpenEditCompanyModal
+            },
+            {
+              id: "delete",
+              label: translateText(["deleteCompany"]),
+              icon: {
+                start: (
+                  <DeleteButtonIcon
+                    width="12px"
+                    height="14px"
+                    fill="var(--color-semantic-red-text)"
+                  />
+                )
+              },
+              activeBehavior:
+                "hover:bg-semantic-red-background text-semantic-red-text",
+              onClick: handleOpenDeleteCompanyModal
+            }
+          ]}
+          className={{
+            anchorElement:
+              "hover:bg-secondary-accent bg-tertiary-background w-9 h-9"
+          }}
+        />
+      }
+    />
+  );
 };
 
 export default CompanySidePanel;
