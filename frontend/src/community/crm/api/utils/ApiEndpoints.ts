2897be9f25d270037d2f21ad90cd2f76536fc27d
sanjula-rtc <sanjula.lakpahana@rootcode.io>
2026-06-11 10:46:30 +0530

    api: add EDIT_CONTACT endpoint to contactEndpoints

--- frontend/src/community/crm/api/utils/ApiEndpoints.ts
+++ frontend/src/community/crm/api/utils/ApiEndpoints.ts
@@ -10,6 +10,7 @@ export const contactEndpoints = {
   GET_CONTACT_METRICS: `${moduleAPIPath.CRM}/contact/metrics`,
   GET_COMPANIES: `${moduleAPIPath.CRM}/company/lookup`,
   CREATE_CONTACT: `${moduleAPIPath.CRM}/contact`,
+  EDIT_CONTACT: (id: number) => `${moduleAPIPath.CRM}/contact/${id}`,
   CONTACT_LOOKUP: `${moduleAPIPath.CRM}/contact/lookup`,
   OWNER_LOOKUP: `${moduleAPIPath.CRM}/contact/owners`
 };
