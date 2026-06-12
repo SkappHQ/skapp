64495f9b9822ccb35e18079a0879eb0e78c43cfb
Shakila Kamalasena <shakila.kamalasena@rootcode.io>
2026-06-12 17:20:22 +0530

    Add ON DELETE CASCADE to employee_id foreign key constraint in employee_skill table

--- backend/src/main/resources/community/db/changelog/ddl-script/people-ddl-script-v1-create-table-ppl-employee-skill.sql
+++ backend/src/main/resources/community/db/changelog/ddl-script/people-ddl-script-v1-create-table-ppl-employee-skill.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `ppl_employee_skill`
     `skill_id`    bigint       NOT NULL,
     `skill_type`  VARCHAR(255) NOT NULL,
     CONSTRAINT `PK_ppl_employee_skill` PRIMARY KEY (`employee_id`, `skill_id`, `skill_type`),
-    CONSTRAINT `FK_ppl_employee_skill_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`)
+    CONSTRAINT `FK_ppl_employee_skill_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`) ON DELETE CASCADE
 );
 
 -- rollback DROP TABLE `ppl_employee_skill`;
