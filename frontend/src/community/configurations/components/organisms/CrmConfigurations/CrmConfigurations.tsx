185047c122d63dabf926109e354a5f2ee1b07fa5
sanjula-rtc <sanjula.lakpahana@rootcode.io>
2026-06-15 20:41:13 +0530

    feat: add CrmConfigurations component and integrate into configuration tabs

---
+++ frontend/src/community/configurations/components/organisms/CrmConfigurations/CrmConfigurations.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface CrmConfigurationsProps {}
+
+const CrmConfigurations: React.FC<CrmConfigurationsProps> = () => {
+  return (
+    <div className="crm-configurations">
+      <h1>CRM Configurations</h1>
+    </div>
+  );
+};
+
+export default CrmConfigurations;
