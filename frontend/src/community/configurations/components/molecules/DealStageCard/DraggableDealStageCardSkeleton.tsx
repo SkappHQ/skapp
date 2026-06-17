4db59486f4ac5e69f2473eb8ea263a1719a1f8f5
sanjula-rtc <sanjula.lakpahana@rootcode.io>
2026-06-17 09:28:01 +0530

    Use theme token for deal stage skeleton

--- frontend/src/community/configurations/components/molecules/DealStageCard/DraggableDealStageCardSkeleton.tsx
+++ frontend/src/community/configurations/components/molecules/DealStageCard/DraggableDealStageCardSkeleton.tsx
@@ -1,8 +1,8 @@
 const DraggableDealStageCardSkeletonItem = () => {
   return (
     <div className="h-[60px] flex flex-row gap-[18px] w-full items-center">
-      <div className="h-[24px] w-[24px] bg-gray-200 rounded-[8px]"></div>
-      <div className="h-[60px] w-full bg-gray-200 rounded-[8px]"></div>
+      <div className="h-[24px] w-[24px] bg-secondary-accent rounded-[8px]"></div>
+      <div className="h-[60px] w-full bg-secondary-accent rounded-[8px]"></div>
     </div>
   );
 };
