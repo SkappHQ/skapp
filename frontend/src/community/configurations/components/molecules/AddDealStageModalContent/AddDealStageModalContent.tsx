0523f0173ef6708af2b64b2b6e2529b0e3cacef0
sanjula-rtc <sanjula.lakpahana@rootcode.io>
2026-06-18 16:37:33 +0530

    refactor: remove unused DealStageModalForm components from Add and Edit Deal Stage modals

--- frontend/src/community/configurations/components/molecules/AddDealStageModalContent/AddDealStageModalContent.tsx
+++
@@ -1,7 +0,0 @@
-import DealStageModalForm from "~community/configurations/components/molecules/DealStageModalForm/DealStageModalForm";
-
-const AddDealStageModalContent = () => {
-  return <DealStageModalForm />;
-};
-
-export default AddDealStageModalContent;
