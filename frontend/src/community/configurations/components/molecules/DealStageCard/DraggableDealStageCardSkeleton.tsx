feab77dca6536a962a72808307b03f2788dd771a
sanjula-rtc <sanjula.lakpahana@rootcode.io>
2026-06-18 16:29:41 +0530

    Refactor DraggableDealStageCardSkeleton to inline item rendering for improved performance

--- frontend/src/community/configurations/components/molecules/DealStageCard/DraggableDealStageCardSkeleton.tsx
+++ frontend/src/community/configurations/components/molecules/DealStageCard/DraggableDealStageCardSkeleton.tsx
@@ -1,18 +1,14 @@
-const DraggableDealStageCardSkeletonItem = () => {
-  return (
-    <div className="h-[60px] flex flex-row gap-[18px] w-full items-center">
-      <div className="h-[24px] w-[24px] bg-secondary-accent rounded-[8px]"></div>
-      <div className="h-[60px] w-full bg-secondary-accent rounded-[8px]"></div>
-    </div>
-  );
-};
-
 const DraggableDealStageCardSkeleton = () => {
   return (
     <div className="animate-pulse flex flex-col gap-[18px] w-full">
-      <DraggableDealStageCardSkeletonItem/>
-      <DraggableDealStageCardSkeletonItem/>
-      <DraggableDealStageCardSkeletonItem/>
+      {Array.from({ length: 3 }).map(() => (
+        <div
+          className="h-[60px] flex flex-row gap-[18px] w-full items-center"
+        >
+          <div className="h-[24px] w-[24px] bg-secondary-accent rounded-[8px]"></div>
+          <div className="h-[60px] w-full bg-secondary-accent rounded-[8px]"></div>
+        </div>
+      ))}
     </div>
   );
 };
