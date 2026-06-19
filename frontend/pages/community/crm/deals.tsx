870e995cede599337ce838ec449e59b2d5ae9cd6
dulsara-manakal <dulsara.manakal@rootcode.io>
2026-06-19 08:41:02 +0530

    remove the unused imports

--- frontend/pages/community/crm/deals.tsx
+++ frontend/pages/community/crm/deals.tsx
@@ -1,7 +1,6 @@
 import { NextPage } from "next";
 
 import ContentLayout from "~community/common/components/templates/ContentLayout/ContentLayout";
-import { ZIndexEnums } from "~community/common/enums/CommonEnums";
 import { useTranslator } from "~community/common/hooks/useTranslator";
 import { IconName } from "~community/common/types/IconTypes";
 import DealsSection from "~community/crm/components/organisms/DealsSection/DealsSection";
