b045b304bc2e8fdae5c437f5caf214337a871836
anusham <anusha.mahindarathne@rootcode.io>
2026-06-19 14:08:34 +0530

    feat: remove DealsKanban component and its associated file

--- frontend/pages/community/crm/deals-kanban.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from "next";
-
-import DealsKanbanBoard from "~community/crm/components/organisms/DealsKanbanBoard/DealsKanbanBoard";
-
-const DealsKanban: NextPage = () => {
-  return <DealsKanbanBoard />;
-};
-
-export default DealsKanban;
