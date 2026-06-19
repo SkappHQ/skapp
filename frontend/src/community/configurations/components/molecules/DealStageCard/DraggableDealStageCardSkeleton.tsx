89566ab904ca9b60e82d67c3b3876713a60dff6b
sanjula-rtc <sanjula.lakpahana@rootcode.io>
2026-06-19 11:02:52 +0530

    Fix key assignment in DraggableDealStageCardSkeleton mapping for unique identifiers

--- frontend/src/community/configurations/components/molecules/DealStageCard/DraggableDealStageCardSkeleton.tsx
+++ frontend/src/community/configurations/components/molecules/DealStageCard/DraggableDealStageCardSkeleton.tsx
@@ -1,8 +1,9 @@
 const DraggableDealStageCardSkeleton = () => {
   return (
     <div className="animate-pulse flex flex-col gap-[18px] w-full">
-      {Array.from({ length: 3 }).map(() => (
+      {Array.from({ length: 3 }).map((_, index) => (
         <div
+          key={index}
           className="h-[60px] flex flex-row gap-[18px] w-full items-center"
         >
           <div className="h-[24px] w-[24px] bg-secondary-accent rounded-[8px]"></div>
