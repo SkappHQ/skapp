982dc17fc70e7021601bb6afde566ebf5d94e398
anusham <anusha.mahindarathne@rootcode.io>
2026-06-19 14:05:46 +0530

    feat: add CRM routes with required authentication levels and subtrees

--- frontend/src/community/common/utils/data/routes.ts
+++ frontend/src/community/common/utils/data/routes.ts
@@ -2,7 +2,8 @@ import ROUTES from "~community/common/constants/routes";
 import {
   AdminTypes,
   EmployeeTypes,
-  ManagerTypes, // RepresentativeTypes,
+  ManagerTypes,
+  RepresentativeTypes,
   SenderTypes
 } from "~community/common/types/AuthTypes";
 import { IconName } from "~community/common/types/IconTypes";
@@ -197,71 +198,71 @@ const routes = [
       }
     ]
   },
-  // {
-  //   id: "4",
-  //   name: "CRM",
-  //   url: ROUTES.CRM.BASE,
-  //   icon: IconName.HAND_SHAKE_ICON,
-  //   hasSubTree: true,
-  //   requiredAuthLevel: [
-  //     AdminTypes.SUPER_ADMIN,
-  //     AdminTypes.CRM_ADMIN,
-  //     ManagerTypes.CRM_SALES_MANAGER,
-  //     RepresentativeTypes.CRM_SALES_REPRESENTATIVE
-  //   ],
-  //   subTree: [
-  //     {
-  //       id: "4A",
-  //       name: "Contacts",
-  //       url: ROUTES.CRM.CONTACTS,
-  //       hasSubTree: false,
-  //       requiredAuthLevel: [
-  //         AdminTypes.SUPER_ADMIN,
-  //         AdminTypes.CRM_ADMIN,
-  //         ManagerTypes.CRM_SALES_MANAGER,
-  //         RepresentativeTypes.CRM_SALES_REPRESENTATIVE
-  //       ]
-  //     },
-  //     {
-  //       id: "4B",
-  //       name: "Companies",
-  //       url: ROUTES.CRM.COMPANIES,
-  //       hasSubTree: false,
-  //       requiredAuthLevel: [
-  //         AdminTypes.SUPER_ADMIN,
-  //         AdminTypes.CRM_ADMIN,
-  //         ManagerTypes.CRM_SALES_MANAGER,
-  //         RepresentativeTypes.CRM_SALES_REPRESENTATIVE
-  //       ]
-  //     },
-  //     {
-  //       id: "4C",
-  //       name: "Deals",
-  //       url: ROUTES.CRM.DEALS,
-  //       hasSubTree: false,
-  //       requiredAuthLevel: [
-  //         AdminTypes.SUPER_ADMIN,
-  //         AdminTypes.CRM_ADMIN,
-  //         ManagerTypes.CRM_SALES_MANAGER,
-  //         RepresentativeTypes.CRM_SALES_REPRESENTATIVE
-  //       ]
-  //     },
-  //     {
-  //       id: "4D",
-  //       name: "Tasks",
-  //       url: ROUTES.CRM.TASKS,
-  //       hasSubTree: false,
-  //       requiredAuthLevel: [
-  //         AdminTypes.SUPER_ADMIN,
-  //         AdminTypes.CRM_ADMIN,
-  //         ManagerTypes.CRM_SALES_MANAGER,
-  //         RepresentativeTypes.CRM_SALES_REPRESENTATIVE
-  //       ]
-  //     }
-  //   ]
-  // },
   {
     id: "4",
+    name: "CRM",
+    url: ROUTES.CRM.BASE,
+    icon: IconName.HAND_SHAKE_ICON,
+    hasSubTree: true,
+    requiredAuthLevel: [
+      AdminTypes.SUPER_ADMIN,
+      AdminTypes.CRM_ADMIN,
+      ManagerTypes.CRM_SALES_MANAGER,
+      RepresentativeTypes.CRM_SALES_REPRESENTATIVE
+    ],
+    subTree: [
+      {
+        id: "4A",
+        name: "Contacts",
+        url: ROUTES.CRM.CONTACTS,
+        hasSubTree: false,
+        requiredAuthLevel: [
+          AdminTypes.SUPER_ADMIN,
+          AdminTypes.CRM_ADMIN,
+          ManagerTypes.CRM_SALES_MANAGER,
+          RepresentativeTypes.CRM_SALES_REPRESENTATIVE
+        ]
+      },
+      {
+        id: "4B",
+        name: "Companies",
+        url: ROUTES.CRM.COMPANIES,
+        hasSubTree: false,
+        requiredAuthLevel: [
+          AdminTypes.SUPER_ADMIN,
+          AdminTypes.CRM_ADMIN,
+          ManagerTypes.CRM_SALES_MANAGER,
+          RepresentativeTypes.CRM_SALES_REPRESENTATIVE
+        ]
+      },
+      {
+        id: "4C",
+        name: "Deals",
+        url: ROUTES.CRM.DEALS,
+        hasSubTree: false,
+        requiredAuthLevel: [
+          AdminTypes.SUPER_ADMIN,
+          AdminTypes.CRM_ADMIN,
+          ManagerTypes.CRM_SALES_MANAGER,
+          RepresentativeTypes.CRM_SALES_REPRESENTATIVE
+        ]
+      },
+      {
+        id: "4D",
+        name: "Tasks",
+        url: ROUTES.CRM.TASKS,
+        hasSubTree: false,
+        requiredAuthLevel: [
+          AdminTypes.SUPER_ADMIN,
+          AdminTypes.CRM_ADMIN,
+          ManagerTypes.CRM_SALES_MANAGER,
+          RepresentativeTypes.CRM_SALES_REPRESENTATIVE
+        ]
+      }
+    ]
+  },
+  {
+    id: "5",
     name: "Sign",
     url: ROUTES.SIGN.BASE,
     icon: IconName.DOCUMENTS_ICON,
@@ -321,7 +322,7 @@ const routes = [
     ]
   },
   {
-    id: "5",
+    id: "6",
     name: "Projects",
     url: ROUTES.PROJECTS.BASE,
     icon: IconName.PROJECTS_ICON,
@@ -368,7 +369,7 @@ const routes = [
     ]
   },
   {
-    id: "6",
+    id: "7",
     name: "Invoices",
     url: ROUTES.INVOICE.BASE,
     icon: IconName.INVOICE_ICON,
