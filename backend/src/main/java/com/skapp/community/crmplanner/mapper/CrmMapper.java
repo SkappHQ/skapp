12abb5f7d32563f5aa42453cb93cd54c2c209c87
anusham <anusha.mahindarathne@rootcode.io>
2026-07-03 11:16:32 +0530

    feat: CrmDealResponseDto updated

--- backend/src/main/java/com/skapp/community/crmplanner/mapper/CrmMapper.java
+++ backend/src/main/java/com/skapp/community/crmplanner/mapper/CrmMapper.java
@@ -38,8 +38,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface CrmMapper {
 
-	@Mapping(target = "stageName", source = "stage.name")
-	@Mapping(target = "stageColor", source = "stage.color")
+	@Mapping(target = "stage", source = "stage")
 	@Mapping(target = "companyName", source = "company.name")
 	@Mapping(target = "contactName", source = "contact.name")
 	@Mapping(target = "owner", source = "owner")
