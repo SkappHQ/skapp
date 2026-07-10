4b38230c537a5027817c6994bdd6b781d98f6bc7
dulsara-manakal <dulsara.manakal@rootcode.io>
2026-07-10 13:22:02 +0530

    test: add unit tests for isValidCompanyWebsiteUrl function

--- frontend/src/community/crm/utils/companyValidations.ts
+++ frontend/src/community/crm/utils/companyValidations.ts
@@ -1,12 +1,13 @@
 import * as Yup from "yup";
 
 import { characterLengths } from "~community/common/constants/stringConstants";
-import {
-  isValidPhoneNumber,
-  isValidWebsiteUrl
-} from "~community/common/regex/regexPatterns";
-import { CrmIndustryEnum } from "~community/crm/enums/common";
+import { isValidPhoneNumber } from "~community/common/regex/regexPatterns";
 import { TranslatorFunctionType } from "~community/common/types/CommonTypes";
+import { CrmIndustryEnum } from "~community/crm/enums/common";
+
+export function isValidCompanyWebsiteUrl(): RegExp {
+  return /^(https:\/\/)?(www\.)?[a-z0-9-]{1,63}(\.[a-z0-9-]{1,63}){0,9}\.[a-z]{2,63}(\/[^\s?#]*)?$/i;
+}
 
 export const addCompanyValidations = (translator: TranslatorFunctionType) =>
   Yup.object().shape({
@@ -35,8 +36,9 @@ export const addCompanyValidations = (translator: TranslatorFunctionType) =>
       .nullable()
       .optional()
       .transform((v) => (v === "" ? null : v))
-      .matches(isValidWebsiteUrl(), {
-        message: translator(["validations", "website"])
+      .matches(isValidCompanyWebsiteUrl(), {
+        message: translator(["validations", "website"]),
+        excludeEmptyString: true
       })
       .max(
         characterLengths.CHARACTER_LENGTH,
