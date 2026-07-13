45c769e1712be6b699074d127057cd3398a746d8
dulsara-manakal <dulsara.manakal@rootcode.io>
2026-07-13 16:29:05 +0530

    refactor: define interface for OwnerDropdownItem props

--- frontend/src/community/crm/components/atoms/OwnerDropdownItem/OwnerDropdownItem.tsx
+++ frontend/src/community/crm/components/atoms/OwnerDropdownItem/OwnerDropdownItem.tsx
@@ -3,7 +3,11 @@ import { FC } from "react";
 import OwnerAvatarChip from "~community/crm/components/atoms/OwnerAvatarChip/OwnerAvatarChip";
 import { CrmOwner } from "~community/crm/types/CommonTypes";
 
-const OwnerDropdownItem: FC<{ owner: CrmOwner }> = ({ owner }) => (
+interface OwnerDropdownItemProps {
+  owner: CrmOwner;
+}
+
+const OwnerDropdownItem: FC<OwnerDropdownItemProps> = ({ owner }) => (
   <OwnerAvatarChip id={String(owner.employeeId)} owner={owner} />
 );
 
