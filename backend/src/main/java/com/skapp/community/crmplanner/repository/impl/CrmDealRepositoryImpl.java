66f6d81080b913c5b57b54f0a77e5391a0f71e7b
Yasindu Gamage <yasindu.gamage@rootcode.io>
2026-07-13 16:31:58 +0530

    fix: update CrmDealResponseDto mapping to use crmMapper in CrmDealServiceImpl

--- backend/src/main/java/com/skapp/community/crmplanner/repository/impl/CrmDealRepositoryImpl.java
+++ backend/src/main/java/com/skapp/community/crmplanner/repository/impl/CrmDealRepositoryImpl.java
@@ -243,8 +243,6 @@ public class CrmDealRepositoryImpl implements CrmDealRepository {
 		CriteriaQuery<CrmDeal> query = cb.createQuery(CrmDeal.class);
 		Root<CrmDeal> deal = query.from(CrmDeal.class);
 		deal.fetch(CrmDeal_.stage, JoinType.INNER);
-		deal.fetch(CrmDeal_.company, JoinType.LEFT);
-		deal.fetch(CrmDeal_.contact, JoinType.LEFT);
 		deal.fetch(CrmDeal_.owner, JoinType.INNER);
 
 		query.where(cb.equal(deal.get(CrmDeal_.id), id), cb.isFalse(deal.get(CrmDeal_.isDeleted)));
