41bff4f0bc2cf1f75d32658cda594c97402dc3e5
Akila Silva <akila.silva@rootcode.io>
2026-07-15 10:23:06 +0530

    refactor(attendance): use text for time_record source columns

--- backend/src/main/resources/community/db/changelog/ddl-script/time-ddl-script-v1-alter-table-time-record-add-source-columns.sql
+++ backend/src/main/resources/community/db/changelog/ddl-script/time-ddl-script-v1-alter-table-time-record-add-source-columns.sql
@@ -2,7 +2,7 @@
 
 -- changeset akila:00121_alter_table_time_record_add_clock_in_out_source_columns
 ALTER TABLE `time_record`
-    ADD COLUMN `clock_in_source` varchar(20) NULL,
-    ADD COLUMN `clock_out_source` varchar(20) NULL;
+    ADD COLUMN `clock_in_source` text NULL,
+    ADD COLUMN `clock_out_source` text NULL;
 
 -- rollback ALTER TABLE `time_record` DROP COLUMN `clock_in_source`, DROP COLUMN `clock_out_source`;
