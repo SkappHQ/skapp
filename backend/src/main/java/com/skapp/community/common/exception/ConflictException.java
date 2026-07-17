f5e0000de7206900c784470dd088863442a6bafc
hasalarootcode <hasala.abhilasha@rootcode.io>
2026-07-17 08:25:47 +0530

    refactor: remove outdated comment from ConflictException class

--- backend/src/main/java/com/skapp/community/common/exception/ConflictException.java
+++ backend/src/main/java/com/skapp/community/common/exception/ConflictException.java
@@ -2,11 +2,6 @@ package com.skapp.community.common.exception;
 
 import com.skapp.community.common.constant.MessageConstant;
 
-/**
- * Thrown when a request conflicts with the current state of the resource (e.g. duplicate
- * unique values or concurrent modifications). Mapped to HTTP 409 by
- * {@link GlobalExceptionHandler}.
- */
 public class ConflictException extends ModuleException {
 
 	public ConflictException(MessageConstant messageKey) {
