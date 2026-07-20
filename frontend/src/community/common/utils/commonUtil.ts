db8301e1f30238630bb750149115d4a290c0d178
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-07-20 14:57:11 +0530

    refactoring

--- frontend/src/community/common/utils/commonUtil.ts
+++ frontend/src/community/common/utils/commonUtil.ts
@@ -566,9 +566,9 @@ export const isEnterpriseMode = (): boolean => {
   return process.env.NEXT_PUBLIC_MODE === appModes.ENTERPRISE;
 };
 
-const IOS_USER_AGENT_REGEX = /iPhone|iPad|iPod/i;
-const ANDROID_USER_AGENT_REGEX = /Android/i;
-const MAC_USER_AGENT_REGEX = /Macintosh/i;
+const IOS_USER_AGENT_REGEX = /iPhone|iPad|iPod/;
+const ANDROID_USER_AGENT_REGEX = /Android/;
+const MAC_USER_AGENT_REGEX = /Macintosh/;
 
 export const isMobileDevice = (): boolean => {
   return isAndroidDevice() || isIOSDevice();
