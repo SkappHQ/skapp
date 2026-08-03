b8497ff1a98a40c49b4dcc4bf9c9ba6c870234d0
gaindurootcode <gaindu.nuhansith@rootcode.io>
2026-08-03 16:55:41 +0530

    feat: add birthday celebration panel component

---
+++ frontend/src/community/people/components/molecules/BirthdayCelebrationPanel/BirthdayCelebrationPanel.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import BirthdayCelebration from "~community/people/assets/images/BirthdayCelebration";
+
+const BirthdayCelebrationPanel: FC = () => (
+  <div className="relative h-full w-full bg-white">
+    <BirthdayCelebration />
+  </div>
+);
+
+export default BirthdayCelebrationPanel;
