8c35b83980ab2d7d657591afb5358c9db946878d
gaindurootcode <gaindu.nuhansith@rootcode.io>
2026-08-03 16:54:40 +0530

    feat: add birthday notification enums and constants

---
+++ frontend/src/community/people/constants/birthdayNotificationConstants.ts
@@ -0,0 +1,6 @@
+export const BIRTHDAY_MODAL_CLASS =
+  "relative h-[603px] max-h-[85vh] w-[1107px] max-w-[92vw] overflow-hidden " +
+  "rounded-l-[42.69px] rounded-r-[24px] shadow-[0_20px_40px_rgba(0,0,0,0.15)]";
+
+export const BIRTHDAY_MODAL_CENTERED_CONTENT_CLASS =
+  "flex h-[555px] flex-col items-center justify-center gap-4 text-center";
---
+++ frontend/src/community/people/enums/BirthdayNotificationEnums.ts
@@ -0,0 +1,4 @@
+export enum BirthdayModalVariant {
+  SELF = "self",
+  COLLEAGUE = "colleague"
+}
