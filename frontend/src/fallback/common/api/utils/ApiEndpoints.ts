4c1141f57f488622d69fab79f2907e64b583605e
YasinduRC <yasindu.gamage@rootcode.io>
2026-08-06 23:20:17 +0530

    feat: add sign-out functionality and update authentication endpoints

--- frontend/src/fallback/common/api/utils/ApiEndpoints.ts
+++ frontend/src/fallback/common/api/utils/ApiEndpoints.ts
@@ -1,1 +1,6 @@
-export const authenticationEndpoints = {};
+import { ApiVersions } from "~community/common/constants/configs";
+
+export const authenticationEndpoints = {
+  REFRESH_TOKEN: `${ApiVersions.V1}/auth/session/refresh-token`,
+  SIGNOUT: `${ApiVersions.V1}/auth/session/sign-out`
+};
