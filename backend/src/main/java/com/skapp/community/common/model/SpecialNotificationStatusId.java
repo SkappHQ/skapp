33a6e2f2591629177205f69f39e122a60456f4d1
gaindurootcode <gaindu.nuhansith@rootcode.io>
2026-08-07 15:22:14 +0530

    fix: minor issue fixes

--- backend/src/main/java/com/skapp/community/common/model/SpecialNotificationStatusId.java
+++ backend/src/main/java/com/skapp/community/common/model/SpecialNotificationStatusId.java
@@ -2,36 +2,22 @@ package com.skapp.community.common.model;
 
 import com.skapp.community.common.type.SpecialNotificationType;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class SpecialNotificationStatusId implements Serializable {
 
 	private Long employee;
 
 	private SpecialNotificationType specialNotificationType;
-
-	@Override
-	public boolean equals(Object object) {
-		if (this == object)
-			return true;
-		if (!(object instanceof SpecialNotificationStatusId that))
-			return false;
-		return Objects.equals(getEmployee(), that.getEmployee())
-				&& getSpecialNotificationType() == that.getSpecialNotificationType();
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(getEmployee(), getSpecialNotificationType());
-	}
 
 }
--- backend/src/main/resources/community/db/changelog/ddl-script/common-ddl-script-v1-create-table-com-special-notification-status.sql
+++ backend/src/main/resources/community/db/changelog/ddl-script/common-ddl-script-v1-create-table-com-special-notification-status.sql
@@ -6,15 +6,26 @@ CREATE TABLE IF NOT EXISTS `com_special_notification_status`
     `employee_id`               bigint       NOT NULL,
     `special_notification_type` varchar(255) NOT NULL,
     `last_viewed_date`          date         NOT NULL,
-    `created_by`                text                  DEFAULT NULL,
-    `created_date`              datetime(6)           DEFAULT NULL,
-    `last_modified_by`          text                  DEFAULT NULL,
-    `last_modified_date`        datetime(6)           DEFAULT NULL,
-    CONSTRAINT `PK_com_special_notification_status` PRIMARY KEY (`employee_id`, `special_notification_type`),
+    `created_by` text,
+    `created_date` datetime
+(
+    6
+),
+    `last_modified_by` text,
+    `last_modified_date` datetime
+(
+    6
+),
+    PRIMARY KEY
+(
+    `employee_id`,
+    `special_notification_type`
+),
     CONSTRAINT `FK_com_special_notification_status_employee_employee_id` FOREIGN KEY
 (
     `employee_id`
-) REFERENCES `employee`
+)
+    REFERENCES `employee`
 (
     `employee_id`
 )
