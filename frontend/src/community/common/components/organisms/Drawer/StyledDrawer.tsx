05119365039f96cfd5e76a3de140c1ae2db2165b
umeshaliyanage <umesha.liyanage@rootcode.io>
2026-08-10 14:57:40 +0530

    fix: add getDrawerWidth to improve content reflow

--- frontend/src/community/common/components/organisms/Drawer/StyledDrawer.tsx
+++ frontend/src/community/common/components/organisms/Drawer/StyledDrawer.tsx
@@ -11,27 +11,27 @@ interface StyledDrawerProps {
 export const StyledDrawer = styled(Drawer, {
   shouldForwardProp: (prop) => prop !== "isBelow600" && prop !== "isBelow1024"
 })<StyledDrawerProps>(({ open, isBelow600, isBelow1024 }) => {
-  const getPaperWidth = () => {
+  const getDrawerWidth = () => {
     if (isBelow600) return open ? "100%" : "0%";
     if (isBelow1024) return open ? "17.75rem" : "0rem";
     return open ? "17.75rem" : "4.6rem";
   };
 
   return {
-    "&.MuiDrawer-docked": !isBelow1024
-      ? {
-          width: open ? "17.75rem" : "4.6rem",
+    "&.MuiDrawer-docked": isBelow600
+      ? {}
+      : {
+          width: getDrawerWidth(),
           height: "100dvh",
           transition: `width ${DRAWER_ANIMATION_DURATION} ease`
-        }
-      : {},
+        },
     "& .MuiDrawer-paper": {
       overflowY: "visible",
       overflowX: "hidden",
       transition: `width ${DRAWER_ANIMATION_DURATION} ease`,
       boxShadow:
         "0px 4px 10px 0px rgba(0, 0, 0, 0.06), 0px 40px 24px 0px rgba(0, 0, 0, 0.03), 0px 71px 28px 0px rgba(0, 0, 0, 0.01), 0px 110px 31px 0px rgba(0, 0, 0, 0)",
-      width: getPaperWidth(),
+      width: getDrawerWidth(),
       ...(!isBelow1024 && {
         "&:hover .MuiIconButton-root": {
           opacity: 1,
