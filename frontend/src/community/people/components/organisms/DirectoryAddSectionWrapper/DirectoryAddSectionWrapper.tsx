81e0683eeac1d7576d1b7b2907618373f749794d
umeshaliyanage <umesha.liyanage@rootcode.io>
2026-08-11 17:53:14 +0530

    fix: add use effect to reset the state once the flow closes

--- frontend/src/community/people/components/organisms/DirectoryAddSectionWrapper/DirectoryAddSectionWrapper.tsx
+++ frontend/src/community/people/components/organisms/DirectoryAddSectionWrapper/DirectoryAddSectionWrapper.tsx
@@ -1,7 +1,10 @@
 import { Box } from "@mui/material";
+import { useEffect } from "react";
 
 import StepperComponent from "~community/common/components/molecules/Stepper/Stepper";
 import useStepper from "~community/people/hooks/useStepper";
+import { usePeopleStore } from "~community/people/store/store";
+import { EditPeopleFormTypes } from "~community/people/types/PeopleEditTypes";
 
 import RouteChangeAreYouSureModal from "../../molecules/RouteChangeAreYouSureModal/RouteChangeAreYouSureModal";
 import PeopleFormSections from "../PeopleFormSections/PeopleFormSections";
@@ -11,6 +14,15 @@ const DirectoryAddSectionWrapper = () => {
   const classes = styles();
 
   const { activeStep, steps } = useStepper();
+
+  const { setCurrentStep, setNextStep } = usePeopleStore((state) => state);
+
+  useEffect(() => {
+    return () => {
+      setCurrentStep(EditPeopleFormTypes.personal);
+      setNextStep(EditPeopleFormTypes.personal);
+    };
+  }, [setCurrentStep, setNextStep]);
 
   return (
     <>
