5d9650cea669ffff17446908713a5f284d1f780b
Deshan <deshan.senarathna@rootcode.io>
2026-08-11 11:33:48 +0530

    fix: revert to previous Constants

--- frontend/src/community/crm/constants/contactConstants.ts
+++ frontend/src/community/crm/constants/contactConstants.ts
@@ -1,7 +1,7 @@
 export const CONTACT_SEARCH_DEBOUNCE_DELAY = 500;
 export const DEFAULT_PAGE_SIZE = 10;
 export const DEFAULT_COMPANY_PAGE_SIZE = 100;
-export const ALL_COMPANIES = "";
+export const ALL_COMPANIES = undefined;
 export const CONTACT_NAME_MAX_LENGTH = 255;
 export const CONTACT_EMAIL_MAX_LENGTH = 100;
 export const CONTACT_NUMBER_MAX_LENGTH = 15;
