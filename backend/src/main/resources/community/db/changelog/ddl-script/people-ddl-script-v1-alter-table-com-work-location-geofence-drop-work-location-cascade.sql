a2121d57a28c9033cf17f92188cae96917f89127
Shakila Kamalasena <shakila.kamalasena@rootcode.io>
2026-08-17 12:34:40 +0530

    Add foreign key constraints for work location in geofence and holiday tables

--- backend/src/main/resources/community/db/changelog/ddl-script/people-ddl-script-v1-alter-table-com-work-location-geofence-drop-work-location-cascade.sql
+++ backend/src/main/resources/community/db/changelog/ddl-script/people-ddl-script-v1-alter-table-com-work-location-geofence-drop-work-location-cascade.sql
@@ -3,8 +3,5 @@
 -- changeset shakila:people-ddl-script-v1-alter-table-com-work-location-geofence-drop-work-location-cascade
 ALTER TABLE `com_work_location_geofence`
     DROP FOREIGN KEY `FK_com_work_location_geofence_com_work_location_id`;
-ALTER TABLE `com_work_location_geofence`
-    ADD CONSTRAINT `FK_com_work_location_geofence_com_work_location_id` FOREIGN KEY (`work_location_id`) REFERENCES `com_work_location` (`id`);
 
--- rollback ALTER TABLE `com_work_location_geofence` DROP FOREIGN KEY `FK_com_work_location_geofence_com_work_location_id`;
 -- rollback ALTER TABLE `com_work_location_geofence` ADD CONSTRAINT `FK_com_work_location_geofence_com_work_location_id` FOREIGN KEY (`work_location_id`) REFERENCES `com_work_location` (`id`) ON DELETE CASCADE;
--- backend/src/main/resources/community/db/changelog/ddl-script/people-ddl-script-v1-alter-table-shr-com-work-location-ppl-holiday-drop-work-location-cascade.sql
+++ backend/src/main/resources/community/db/changelog/ddl-script/people-ddl-script-v1-alter-table-shr-com-work-location-ppl-holiday-drop-work-location-cascade.sql
@@ -3,8 +3,5 @@
 -- changeset shakila:people-ddl-script-v1-alter-table-shr-com-work-location-ppl-holiday-drop-work-location-cascade
 ALTER TABLE `shr_com_work_location_ppl_holiday`
     DROP FOREIGN KEY `FK_shr_com_work_location_ppl_holiday_work_location_id`;
-ALTER TABLE `shr_com_work_location_ppl_holiday`
-    ADD CONSTRAINT `FK_shr_com_work_location_ppl_holiday_work_location_id` FOREIGN KEY (`work_location_id`) REFERENCES `com_work_location` (`id`);
 
--- rollback ALTER TABLE `shr_com_work_location_ppl_holiday` DROP FOREIGN KEY `FK_shr_com_work_location_ppl_holiday_work_location_id`;
 -- rollback ALTER TABLE `shr_com_work_location_ppl_holiday` ADD CONSTRAINT `FK_shr_com_work_location_ppl_holiday_work_location_id` FOREIGN KEY (`work_location_id`) REFERENCES `com_work_location` (`id`) ON DELETE CASCADE;
