a14ad14506812695ae9da9b301911e419b80c0cf
hasalarootcode <hasala.abhilasha@rootcode.io>
2026-08-17 09:50:54 +0530

    feat: remove obsolete carryover expiry date column and related test

--- backend/src/main/resources/community/db/changelog/ddl-script/leave-ddl-script-v1-alter-leave-policy-rename-carryover-expiry-date.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
-
--- changeset HasalaAbhilasha:leave-ddl-script-v1-alter-leave-policy-rename-carryover-expiry-date
-ALTER TABLE `lv_leave_policy`
-    CHANGE COLUMN `carryover_date` `carryover_expiry_date` text;
-
--- rollback ALTER TABLE `lv_leave_policy` CHANGE COLUMN `carryover_expiry_date` `carryover_date` text;
