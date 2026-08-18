52c473dcd7bfdb379674c7291795c6d862a5db9d
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-08-18 09:22:51 +0530

    remove comment

--- backend/src/main/java/com/skapp/community/timeplanner/type/TimeRequestSort.java
+++ backend/src/main/java/com/skapp/community/timeplanner/type/TimeRequestSort.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 @Getter
 public enum TimeRequestSort {
 
-	// createdDate is inherited from Auditable, which owns the created_date column.
 	REQUESTED_START_TIME("requestedStartTime"), REQUESTED_END_TIME("requestedEndTime"), CREATION_DATE("createdDate");
 
 	private final String sortField;
