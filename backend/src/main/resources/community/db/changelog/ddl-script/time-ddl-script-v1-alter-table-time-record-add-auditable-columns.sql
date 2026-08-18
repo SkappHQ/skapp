f346ab320a7bf0c53b76ca9f5f4fdd46cd9fe539
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-08-18 07:38:18 +0530

    add script to db changelog

--- backend/src/main/resources/community/db/changelog/ddl-script/time-ddl-script-v1-alter-table-time-record-add-auditable-columns.sql
+++ backend/src/main/resources/community/db/changelog/ddl-script/time-ddl-script-v1-alter-table-time-record-add-auditable-columns.sql
@@ -6,7 +6,4 @@ ALTER TABLE `time_request`
     ADD COLUMN    `last_modified_by`   varchar(255)          DEFAULT NULL,
     ADD COLUMN    `last_modified_date` datetime(6)           DEFAULT NULL;
 
--- rollback ALTER TABLE `time_request`
--- DROP COLUMN `created_by`,
--- DROP COLUMN `last_modified_by`,
--- DROP COLUMN `last_modified_date`;
+-- rollback ALTER TABLE `time_request` DROP COLUMN `created_by`, DROP COLUMN `last_modified_by`, DROP COLUMN `last_modified_date`;
