f2bf9d0b989a2ad0c83be9b066876866f0563c93
YasinduRC <yasindu.gamage@rootcode.io>
2026-08-25 09:34:29 +0530

    Refactor CrmTaskSort: Simplify constructor and toString method using Lombok

--- backend/src/main/java/com/skapp/community/crmplanner/type/CrmTaskSort.java
+++ backend/src/main/java/com/skapp/community/crmplanner/type/CrmTaskSort.java
@@ -1,21 +1,14 @@
 package com.skapp.community.crmplanner.type;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum CrmTaskSort {
 
 	DUE_AT("dueAt"), LAST_MODIFIED_DATE("lastModifiedDate");
 
 	private final String sortField;
-
-	CrmTaskSort(String sortField) {
-		this.sortField = sortField;
-	}
-
-	@Override
-	public String toString() {
-		return this.sortField;
-	}
 
 }
