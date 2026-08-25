2051f25408f455f26bebb14eebcb903ad91d972d
ErandiDeSilva_Rootcode <erandi.desilva@rootcode.io>
2026-08-25 12:17:46 +0530

    change varchar to text

--- backend/src/main/resources/community/db/changelog/ddl-script/time-ddl-script-v1-alter-table-time_request-add-auditable-columns.sql
+++ backend/src/main/resources/community/db/changelog/ddl-script/time-ddl-script-v1-alter-table-time_request-add-auditable-columns.sql
@@ -2,8 +2,8 @@
 
 -- changeset ErandiDeSilva:alter_table_add_auditable_columns_to_time_request_table
 ALTER TABLE `time_request`
-    ADD COLUMN    `created_by`         varchar(255)          DEFAULT NULL,
-    ADD COLUMN    `last_modified_by`   varchar(255)          DEFAULT NULL,
-    ADD COLUMN    `last_modified_date` datetime(6)           DEFAULT NULL;
+    ADD COLUMN    `created_by`         text,
+    ADD COLUMN    `last_modified_by`   text,
+    ADD COLUMN    `last_modified_date` datetime(6);
 
 -- rollback ALTER TABLE `time_request` DROP COLUMN `created_by`, DROP COLUMN `last_modified_by`, DROP COLUMN `last_modified_date`;
