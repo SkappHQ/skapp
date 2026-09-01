10e14b70a0041e107c4b74a21baeceebb71a96b3
anusham <anusha.mahindarathne@rootcode.io>
2026-09-01 09:59:49 +0530

    Size crm_deal_list_view column so H2 test schema fits the config

--- backend/src/main/java/com/skapp/community/common/model/UserSettings.java
+++ backend/src/main/java/com/skapp/community/common/model/UserSettings.java
@@ -38,7 +38,7 @@ public class UserSettings {
 	@Convert(converter = JsonTypeConverter.class)
 	private JsonNode notifications;
 
-	@Column(name = "crm_deal_list_view")
+	@Column(name = "crm_deal_list_view", length = 10000)
 	@Convert(converter = JsonTypeConverter.class)
 	private JsonNode crmDealListView;
 
