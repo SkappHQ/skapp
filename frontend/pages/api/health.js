0a02c900c8cafddc07233ce902fb89f3d80e0102
hasalarootcode <hasala.abhilasha@rootcode.io>
2026-09-07 13:26:12 +0530

    refactor: convert to app router

--- frontend/pages/api/health.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import packageJson from "../../package.json";
-
-export default function handler(req, res) {
-  res.status(200).json({ status: "healthy", appVersion: packageJson.version });
-}
